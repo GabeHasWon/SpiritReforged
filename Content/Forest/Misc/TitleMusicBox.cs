3800acca41173211cea3dee5aae53f2f57d062c4
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2025-05-23 20:23:08 -0300

    Spirit title music box

---
+++ Content/Forest/Misc/TitleMusicBox.cs
@@ -0,0 +1,27 @@
+﻿using SpiritReforged.Common.ItemCommon;
+using SpiritReforged.Common.TileCommon.PresetTiles;
+using SpiritReforged.Content.Ocean.Tiles;
+using SpiritReforged.Content.Savanna.Tiles;
+
+namespace SpiritReforged.Content.Forest.Misc;
+
+public class TitleMusicBox : MusicBoxTile
+{
+	public override string MusicPath => "Assets/Music/TitleTheme";
+	public override void Load() { } //Don't autoload an item
+}
+
+public class TitleMusicBoxItem : ModItem
+{
+	public override void SetStaticDefaults()
+	{
+		MusicLoader.AddMusicBox(Mod, MusicLoader.GetMusicSlot(Mod, ModContent.GetInstance<TitleMusicBox>().MusicPath), Type, ModContent.TileType<TitleMusicBox>());
+
+		ItemID.Sets.CanGetPrefixes[Type] = false;
+		ItemID.Sets.ShimmerTransformToItem[Type] = ItemID.MusicBox;
+	}
+
+	public override void SetDefaults() => Item.DefaultToMusicBox(ModContent.TileType<TitleMusicBox>(), 0);
+	public override void AddRecipes() => CreateRecipe().AddIngredient(ItemMethods.AutoItemType<OceanDepthsBox>()).AddIngredient(ItemMethods.AutoItemType<SavannaMusicBox>())
+			.AddIngredient(ItemMethods.AutoItemType<SavannaNightMusicBox>()).AddIngredient(ItemMethods.AutoItemType<SavannaSandstormMusicBox>()).Register();
+}
