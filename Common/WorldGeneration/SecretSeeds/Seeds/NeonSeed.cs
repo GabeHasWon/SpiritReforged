bfca7ea88ba20a04522e546e6165056725bf3c3e
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2025-05-28 02:54:18 -0300

    Start moss secret seed

---
+++ Common/WorldGeneration/SecretSeeds/Seeds/NeonSeed.cs
@@ -0,0 +1,9 @@
+﻿using SpiritReforged.Common.Visuals;
+
+namespace SpiritReforged.Common.WorldGeneration.SecretSeeds.Seeds;
+
+internal class NeonSeed : SecretSeed
+{
+	public override string[] Keys => ["moss", "glowing moss", "krypton", "xenon", "argon", "neon", "helium", "radon", "oganesson"];
+	public override string Icon => DrawHelpers.RequestLocal(GetType(), ModContent.GetInstance<SavannaSeed>().Name + "_Icon"); //DEBUG
+}
--- Content/Underground/Moss/Radon/RadonMossGrayBrick.cs
+++ Content/Underground/Moss/Radon/RadonMossGrayBrick.cs
@@ -27,15 +27,12 @@ public class RadonMossGrayBrick : GrassTile
 
 	public override void RandomUpdate(int i, int j)
 	{
-		if (SpreadHelper.Spread(i, j, Type, 1, DirtType) && Main.netMode != NetmodeID.SinglePlayer)
-			NetMessage.SendTileSquare(-1, i, j, 3, TileChangeType.None); //Try spread moss
+		SpreadHelper.Spread(i, j, Type, 1, DirtType); //Try spread moss
+		SpreadHelper.Spread(i, j, ModContent.TileType<RadonMoss>(), 1, TileID.Stone); //Also spread to stone
 
-		if (SpreadHelper.Spread(i, j, ModContent.TileType<RadonMoss>(), 1, TileID.Stone) && Main.netMode != NetmodeID.SinglePlayer)
-			NetMessage.SendTileSquare(-1, i, j, 3, TileChangeType.None); //Also spread to stone
-
-		GrowTiles(i, j);
+		GrowPlants(i, j);
 	}
 
-	protected virtual void GrowTiles(int i, int j) => Placer.PlacePlant<RadonPlants>(i, j, Main.rand.Next(RadonPlants.StyleRange));
+	public override void GrowPlants(int i, int j) => Placer.PlacePlant<RadonPlants>(i, j, Main.rand.Next(RadonPlants.StyleRange));
 	public override void ModifyLight(int i, int j, ref float r, ref float g, ref float b) => (r, g, b) = (0.234f, 0.153f, 0.03f);
 }
