1c5b12a9f678b6b6fb869109cd9d02211d7cee50
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2025-05-29 22:25:25 -0300

    Review change

--- Content/Forest/Misc/Bonsai/BonsaiTreeItems.cs
+++ Content/Forest/Misc/Bonsai/BonsaiTreeItems.cs
@@ -33,40 +33,40 @@ public class RubyBonsaiItem : SakuraBonsaiItem
 {
 	public override int Style => 3;
 	public override void SetStaticDefaults() => NPCShopHelper.AddEntry(new NPCShopHelper.ConditionalEntry((shop) 
-		=> shop.NpcType == NPCID.Dryad, new NPCShop.Entry(Type, Condition.InBelowSurface, Condition.PlayerCarriesItem(ItemID.Ruby))));
+		=> shop.NpcType == NPCID.Dryad, new NPCShop.Entry(Type, Condition.PlayerCarriesItem(ItemID.Ruby))));
 }
 
 public class DiamondBonsaiItem : SakuraBonsaiItem
 {
 	public override int Style => 4;
 	public override void SetStaticDefaults() => NPCShopHelper.AddEntry(new NPCShopHelper.ConditionalEntry((shop) 
-		=> shop.NpcType == NPCID.Dryad, new NPCShop.Entry(Type, Condition.InBelowSurface, Condition.PlayerCarriesItem(ItemID.Diamond))));
+		=> shop.NpcType == NPCID.Dryad, new NPCShop.Entry(Type, Condition.PlayerCarriesItem(ItemID.Diamond))));
 }
 
 public class EmeraldBonsaiItem : SakuraBonsaiItem
 {
 	public override int Style => 5;
 	public override void SetStaticDefaults() => NPCShopHelper.AddEntry(new NPCShopHelper.ConditionalEntry((shop) 
-		=> shop.NpcType == NPCID.Dryad, new NPCShop.Entry(Type, Condition.InBelowSurface, Condition.PlayerCarriesItem(ItemID.Emerald))));
+		=> shop.NpcType == NPCID.Dryad, new NPCShop.Entry(Type, Condition.PlayerCarriesItem(ItemID.Emerald))));
 }
 
 public class SapphireBonsaiItem : SakuraBonsaiItem
 {
 	public override int Style => 6;
 	public override void SetStaticDefaults() => NPCShopHelper.AddEntry(new NPCShopHelper.ConditionalEntry((shop) 
-		=> shop.NpcType == NPCID.Dryad, new NPCShop.Entry(Type, Condition.InBelowSurface, Condition.PlayerCarriesItem(ItemID.Sapphire))));
+		=> shop.NpcType == NPCID.Dryad, new NPCShop.Entry(Type, Condition.PlayerCarriesItem(ItemID.Sapphire))));
 }
 
 public class TopazBonsaiItem : SakuraBonsaiItem
 {
 	public override int Style => 7;
 	public override void SetStaticDefaults() => NPCShopHelper.AddEntry(new NPCShopHelper.ConditionalEntry((shop) 
-		=> shop.NpcType == NPCID.Dryad, new NPCShop.Entry(Type, Condition.InBelowSurface, Condition.PlayerCarriesItem(ItemID.Topaz))));
+		=> shop.NpcType == NPCID.Dryad, new NPCShop.Entry(Type, Condition.PlayerCarriesItem(ItemID.Topaz))));
 }
 
 public class AmethystBonsaiItem : SakuraBonsaiItem
 {
 	public override int Style => 8;
 	public override void SetStaticDefaults() => NPCShopHelper.AddEntry(new NPCShopHelper.ConditionalEntry((shop) 
-		=> shop.NpcType == NPCID.Dryad, new NPCShop.Entry(Type, Condition.InBelowSurface, Condition.PlayerCarriesItem(ItemID.Amethyst))));
+		=> shop.NpcType == NPCID.Dryad, new NPCShop.Entry(Type, Condition.PlayerCarriesItem(ItemID.Amethyst))));
 }
