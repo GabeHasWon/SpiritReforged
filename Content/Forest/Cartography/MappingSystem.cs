21f51e295584b1c88505a50521594f32a04b5746
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2025-06-04 17:46:53 -0300

    Functional system

--- Content/Forest/Cartography/MappingSystem.cs
+++ Content/Forest/Cartography/MappingSystem.cs
@@ -6,107 +6,101 @@ namespace SpiritReforged.Content.Forest.Cartography;
 
 public class MappingSystem : ModSystem
 {
-	public static int Buffer { get; private set; }
-	public static int BufferMax => Main.maxTilesX * Main.maxTilesY / 12000;
-
 	internal static WorldMap RecordedMap;
-
-	public static void SetBuffer() => Buffer = BufferMax;
 
 	public override void ClearWorld() => RecordedMap = null;
 
-	public override void PreUpdatePlayers()
-	{
-		if (Buffer > 0 && Main.netMode != NetmodeID.SinglePlayer)
-		{
-			if (Buffer == 1)
-			{
-				Main.NewText("Finished sync (DEBUG)");
-				Main.refreshMap = true;
-			}
-
-			new SyncMapData(RecordedMap).Send();
-			Buffer--;
-		}
-	}
-
 	public static void SetMap()
 	{
-		if (Buffer > 0 || Main.netMode == NetmodeID.SinglePlayer)
+		if (Main.netMode != NetmodeID.MultiplayerClient)
 			return;
 
-		RecordedMap ??= Main.Map;
-
-		for (int x = 0; x < RecordedMap.MaxWidth; x++)
+		for (int x = 0; x < Main.Map.MaxWidth; x++)
 		{
-			for (int y = 0; y < RecordedMap.MaxHeight; y++)
+			for (int y = 0; y < Main.Map.MaxHeight; y++)
 			{
-				var tileToSend = Main.Map[x, y];
-
-				if (tileToSend.Light > RecordedMap[x, y].Light)
-					RecordedMap.SetTile(x, y, ref tileToSend);
+				var t = Main.Map[x, y];
+				
+				if (t.Light != 0)
+					SyncMapData.Queue.Enqueue(new((ushort)x, (ushort)y, t));
 			}
 		}
 
-		Buffer = BufferMax;
-		//new StartSyncMapData().Send();
+		while (SyncMapData.Queue.Count > 0)
+		{
+			new SyncMapData().Send();
+		}
+
+		Main.refreshMap = true;
 	}
 }
 
 internal class SyncMapData : PacketData
 {
-	private readonly WorldMap _map;
+	public readonly record struct QueueData(ushort X, ushort Y, MapTile Tile)
+	{
+		public readonly ushort X = X;
+		public readonly ushort Y = Y;
+		public readonly MapTile Tile = Tile;
+	}
+
+	public static readonly Queue<QueueData> Queue = [];
 
 	public SyncMapData() { }
-	public SyncMapData(WorldMap map) => _map = map;
 
 	public override void OnReceive(BinaryReader reader, int whoAmI)
 	{
-		MappingSystem.RecordedMap ??= new(Main.maxTilesX, Main.maxTilesY);
-
-		ScanOverMap(MappingSystem.RecordedMap, (x, y) =>
+		ushort count = reader.ReadUInt16();
+		for (int i = 0; i < count; i++)
 		{
+			ushort x = reader.ReadUInt16();
+			ushort y = reader.ReadUInt16();
+
+			ushort type = reader.ReadUInt16();
 			byte light = reader.ReadByte();
+			byte color = reader.ReadByte();
 
-			var tileToPost = MapHelper.CreateMapTile(x, y, light);
-			MappingSystem.RecordedMap.SetTile(x, y, ref tileToPost);
-		});
+			var t = MapTile.Create(type, light, color);
 
-		if (Main.netMode == NetmodeID.Server) //Relay to other clients
-			new SyncMapData(MappingSystem.RecordedMap).Send(ignoreClient: whoAmI);
-	}
+			if (Main.netMode == NetmodeID.Server)
+			{
+				MappingSystem.RecordedMap ??= new(Main.maxTilesX, Main.maxTilesY);
+				MappingSystem.RecordedMap.SetTile(x, y, ref t);
 
-	public override void OnSend(ModPacket modPacket) => ScanOverMap(_map, (x, y) => WriteSingleMapTile(modPacket, _map[x, y]));
-
-	private static void ScanOverMap(WorldMap map, Action<int, int> action)
-	{
-		int maxTiles = (map.MaxWidth - 1) * (map.MaxHeight - 1);
-
-		for (int t = Segment(); t < Segment(-1); t++)
-		{
-			int x = t / map.MaxHeight;
-			int y = t % map.MaxHeight;
-
-			action.Invoke(x, y);
+				Queue.Enqueue(new(x, y, t)); //Enqueue again because the server needs to relay
+			}
+			else
+			{
+				Main.Map.SetTile(x, y, ref t);
+			}
 		}
 
-		int Segment(int move = 0) => (int)(maxTiles * (1f - (float)(MappingSystem.Buffer - move) / MappingSystem.BufferMax));
-	}
-
-	private static void WriteSingleMapTile(ModPacket modPacket, MapTile tile) => modPacket.Write(tile.Light); //Only write light levels because we can infer the rest
-}
-
-internal class StartSyncMapData : PacketData
-{
-	public StartSyncMapData() { }
-
-	public override void OnReceive(BinaryReader reader, int whoAmI)
-	{
-		MappingSystem.SetBuffer();
+		if (Main.netMode == NetmodeID.Server)
+			new SyncMapData().Send(); //Relay to all clients
 	}
 
 	public override void OnSend(ModPacket modPacket)
 	{
-		MappingSystem.SetBuffer();
+		ushort count = (ushort)Math.Min(150, Queue.Count); //Restricts packet size to avoid hitting the limit
+		ushort currentCount = 0;
+
+		modPacket.Write(count);
+
+		foreach (var data in Queue)
+		{
+			if (currentCount++ > count)
+				break;
+
+			modPacket.Write(data.X);
+			modPacket.Write(data.Y);
+
+			var t = data.Tile;
+			modPacket.Write(t.Type);
+			modPacket.Write(t.Light);
+			modPacket.Write(t.Color);
+		}
+
+		for (int i = 0; i < count; i++)
+			Queue.Dequeue();
 	}
 }
