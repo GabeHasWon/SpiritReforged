56569625e645caad583f7b0597aca74ee6b6985d
PhoenixBladez <amanchala13@gmail.com>
2025-06-11 12:23:47 -0700

    more dialogue

--- Content/Vanilla/Misc/VanillaNPCDialogue.cs
+++ Content/Vanilla/Misc/VanillaNPCDialogue.cs
@@ -1,5 +1,8 @@
-﻿using SpiritReforged.Content.Savanna.Biome;
+﻿using SpiritReforged.Common.PlayerCommon;
+using SpiritReforged.Content.Forest.Cartography;
+using SpiritReforged.Content.Savanna.Biome;
 using SpiritReforged.Content.Savanna.DustStorm;
+using SpiritReforged.Content.Underground.Items.BigBombs;
 
 namespace SpiritReforged.Common.NPCCommon;
 
@@ -30,6 +33,65 @@ public class VanillaNPCDialogue : GlobalNPC
 					if (Main.rand.NextBool(2))
 						chat = Language.GetTextValue("Mods.SpiritReforged.NPCs.VanillaDialogue.Stylist.Duststorm1");
 				}
+			}
+		}
+
+		if (npc.type == NPCID.Nurse)
+		{
+			if (player.InModBiome<SavannaBiome>())
+			{
+				if (Main.rand.NextBool(3))
+					chat = Language.GetTextValue("Mods.SpiritReforged.NPCs.VanillaDialogue.Nurse.Savanna");
+
+				if (player.GetModPlayer<DustStormPlayer>().ZoneDustStorm)
+				{
+					if (Main.rand.NextBool(2))
+						chat = Language.GetTextValue("Mods.SpiritReforged.NPCs.VanillaDialogue.Nurse.Duststorm");
+				}
+			}
+		}
+
+		if (npc.type == NPCID.TravellingMerchant)
+		{
+			int cartographer = NPC.FindFirstNPC(ModContent.NPCType<Cartographer>());
+			if (cartographer >= 0)
+			{
+				if (Main.rand.NextBool(3))
+					chat = Language.GetTextValue("Mods.SpiritReforged.NPCs.VanillaDialogue.TravellingMerchant.Cartographer2", Main.npc[cartographer].GivenName);
+			}
+			else
+			{
+				if (Main.rand.NextBool(3))
+					chat = Language.GetTextValue("Mods.SpiritReforged.NPCs.VanillaDialogue.TravellingMerchant.Cartographer1");
+			}
+
+			if (Main.rand.NextBool(4))
+				chat = Language.GetTextValue("Mods.SpiritReforged.NPCs.VanillaDialogue.TravellingMerchant.Discoveries");
+		}
+
+		if (npc.type == NPCID.Guide)
+		{
+			if (Main.rand.NextBool(6))
+				chat = Language.GetTextValue("Mods.SpiritReforged.NPCs.VanillaDialogue.Guide.WorldNPC");
+			
+			if (player.InModBiome<SavannaBiome>())
+			{
+				if (Main.rand.NextBool(3))
+					chat = Language.GetTextValue("Mods.SpiritReforged.NPCs.VanillaDialogue.Guide.Savanna");
+			}
+			else
+			{
+				if (Main.rand.NextBool(6))
+					chat = Language.GetTextValue("Mods.SpiritReforged.NPCs.VanillaDialogue.Guide.Ecotone");
+			}
+		}
+
+		if (npc.type == NPCID.Demolitionist)
+		{
+			if (player.HasItem(ModContent.ItemType<BoomShroom>()) || player.HasEquip<BoomShroom>())
+			{
+				if (Main.rand.NextBool(4))
+					chat = Language.GetTextValue("Mods.SpiritReforged.NPCs.VanillaDialogue.Demolitionist.HasBoomshroom");
 			}
 		}
 	}
