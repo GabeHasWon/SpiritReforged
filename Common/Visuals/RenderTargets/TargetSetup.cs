70d720ab54626897680d0f1e52fafabd9cf2e22c
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2025-06-11 16:06:39 -0300

    Target helper

---
+++ Common/Visuals/RenderTargets/TargetSetup.cs
@@ -0,0 +1,30 @@
+﻿namespace SpiritReforged.Common.Visuals.RenderTargets;
+
+internal class TargetSetup : ILoadable
+{
+	/// <summary> Called with <see cref="On_Main.CheckMonoliths"/>. </summary>
+	public static event Action DrawIntoRendertargets;
+
+	public void Load(Mod mod)
+	{
+		On_Main.CheckMonoliths += static (orig) =>
+		{
+			orig();
+
+			if (!Main.gameMenu && !Main.dedServ)
+				DrawIntoRendertargets?.Invoke();
+		};
+
+		DrawIntoRendertargets += DrawIntoTargets;
+	}
+	public void Unload() { }
+
+	private static void DrawIntoTargets()
+	{
+		foreach (var target in ModTarget2D.Targets)
+		{
+			if (target.Active)
+				target.Prepare(Main.spriteBatch);
+		}
+	}
+}
