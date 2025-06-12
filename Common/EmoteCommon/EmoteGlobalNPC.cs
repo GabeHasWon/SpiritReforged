eddd1eda5518434ab83292243d954d605a794712
Abhay M <amanchala13@gmail.com>
2025-06-12 13:22:05 -0700

    finalize cartographerEmote

--- Common/EmoteCommon/EmoteGlobalNPC.cs
+++ Common/EmoteCommon/EmoteGlobalNPC.cs
@@ -1,3 +1,4 @@
+using SpiritReforged.Content.Forest.Cartography;
 using SpiritReforged.Content.Visuals.Emotes;
 using Terraria.GameContent.UI;
 using Terraria.GameContent.UI.Elements;
@@ -7,8 +8,12 @@ public class EmoteGlobalNPC : GlobalNPC
 {
 	public override int? PickEmote(NPC npc, Player closestPlayer, List<int> emoteList, WorldUIAnchor otherAnchor)
 	{
-		if (Main.rand.NextBool(2))
-			emoteList.Add(ModContent.EmoteBubbleType<CartorgrapherEmote>());
+		int cartographer = NPC.FindFirstNPC(ModContent.NPCType<Cartographer>());
+		if (cartographer >= 0)
+		{
+			if (Main.rand.NextBool(2))
+				emoteList.Add(ModContent.EmoteBubbleType<CartorgrapherEmote>());
+		}
 
 		return base.PickEmote(npc, closestPlayer, emoteList, otherAnchor);
 	}
