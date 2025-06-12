ac679a35bd7e34c79503c664eab3c03502916bdd
PhoenixBladez <amanchala13@gmail.com>
2025-06-12 10:23:53 -0700

    Moss Slime Changes
    - Generally reduced light emitted
    - Added Hit Dusts
    - Added slime lootpool
    - Made glowmasks fullbright to match the mosses
    - Added death particle effects

--- Content/Underground/NPCs/MossSlimes/MossSlime.cs
+++ Content/Underground/NPCs/MossSlimes/MossSlime.cs
@@ -1,4 +1,7 @@
-﻿using Terraria.GameContent.Bestiary;
+﻿using SpiritReforged.Common.Particle;
+using SpiritReforged.Content.Particles;
+using Terraria;
+using Terraria.GameContent.Bestiary;
 
 namespace SpiritReforged.Content.Underground.NPCs.MossSlimes;
 
@@ -17,6 +20,7 @@ internal class MossSlime : ModNPC
 	public override string Texture => "SpiritReforged/Content/Underground/NPCs/MossSlimes/MossSlimeBase";
 
 	protected virtual Vector3 LightColor { get; }
+	protected virtual int DustType { get; }
 	protected virtual int MossType { get; }
 	protected virtual HashSet<int> TileTypes { get; } = [];
 
@@ -58,11 +62,16 @@ internal class MossSlime : ModNPC
 
 	public override bool PreAI()
 	{
-		Lighting.AddLight(NPC.Center, LightColor);
+		Lighting.AddLight(NPC.Center, LightColor * .75f);
 		return true;
 	}
 
-	public override void ModifyNPCLoot(NPCLoot npcLoot) => npcLoot.AddCommon(MossType, 1, 5, 10);
+	public override void ModifyNPCLoot(NPCLoot npcLoot)
+	{
+		npcLoot.AddCommon(ItemID.Gel, 1, 1, 3);
+		npcLoot.AddCommon(ItemID.SlimeStaff, 10000);
+		npcLoot.AddCommon(MossType, 1, 5, 10);
+	}
 
 	public override float SpawnChance(NPCSpawnInfo spawnInfo)
 	{
@@ -114,6 +123,8 @@ internal class MossSlime : ModNPC
 	{
 		if (NPC.type == ModContent.NPCType<HeliumMossSlime>())
 			drawColor = Main.DiscoColor;
+		else
+			drawColor = Color.White;
 
 		Texture2D tex = (drawBack ? BackSpritesById[Type] : FrontSpritesById[Type]).Value;
 		Vector2 position = NPC.Center - screenPos;
@@ -125,6 +136,43 @@ internal class MossSlime : ModNPC
 			var slime = ModContent.GetModNPC(Type) as MossSlime;
 			int heightOff = NPC.frame.Y == 0 ? 0 : 2;
 			Main.DrawItemIcon(spriteBatch, ContentSamples.ItemsByType[slime.MossType], position - new Vector2(0, heightOff), drawColor * 0.5f, 32);
+		}
+	}
+	public override void HitEffect(NPC.HitInfo hit)
+	{
+		if (!Main.dedServ)
+		{
+			bool dead = NPC.life <= 0;
+
+			for (int k = 0; k < 5; k++)
+			{
+				int dust = Dust.NewDust(NPC.position, NPC.width, NPC.height, DustType, 1.5f * hit.HitDirection, -2.5f, 0, default, Main.rand.NextFloat(0.4f, 0.8f));
+
+				if (NPC.type == ModContent.NPCType<HeliumMossSlime>())
+					Main.dust[dust].noGravity = true;				
+
+				if (NPC.type == ModContent.NPCType<LavaMossSlime>() || NPC.type == ModContent.NPCType<OganessonMossSlime>() || NPC.type == ModContent.NPCType<RadonMossSlime>())
+					Main.dust[dust].noLight = true;	
+			}
+
+			for (int k = 0; k < 8; k++)
+				Dust.NewDust(NPC.position, NPC.width, NPC.height, DustID.TintableDust, 2.5f * hit.HitDirection, -2.5f, 0, Color.Black * 0.5f, Main.rand.NextFloat(0.3f, 0.6f));
+
+			if (dead)
+			{
+				for (int k = 0; k < 7; k++)
+				{
+					var start = NPC.BottomLeft + new Vector2(Main.rand.Next(NPC.width), 0);
+					var velocity = new Vector2(Main.rand.NextFloat(-2f, 2f), -2).RotatedByRandom(MathHelper.PiOver4) * Main.rand.NextFloat(0.4f, 1f);
+
+					ParticleHandler.SpawnParticle(new GlowParticle(start, velocity,
+						new Color(LightColor) * 0.85f, Main.rand.NextFloat(0.25f, 0.45f), 40, 8, p =>
+						{
+							p.Velocity.X *= Main.rand.NextFloat(.8f, .9f);
+							p.Velocity.Y *= Main.rand.NextFloat(.96f, .99f);
+						}));
+				}
+			}
 		}
 	}
 }
