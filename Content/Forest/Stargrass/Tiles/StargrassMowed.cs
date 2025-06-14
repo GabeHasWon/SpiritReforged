ad0d41d279abdf7bb242ec2f496f516f46fd6af1
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2025-06-14 11:47:39 -0300

    Mowed Stargrass

---
+++ Content/Forest/Stargrass/Tiles/StargrassMowed.cs
@@ -0,0 +1,36 @@
+﻿using SpiritReforged.Common.Visuals.Glowmasks;
+using SpiritReforged.Common.WorldGeneration;
+
+namespace SpiritReforged.Content.Forest.Stargrass.Tiles;
+
+[AutoloadGlowmask("Method:Content.Forest.Stargrass.Tiles.StargrassTile Glow")]
+public class StargrassMowed : StargrassTile
+{
+	public override void SetStaticDefaults()
+	{
+		base.SetStaticDefaults();
+
+		Main.tileLighted[Type] = true;
+		TileID.Sets.Conversion.Grass[Type] = true;
+
+		RegisterItemDrop(ItemID.DirtBlock);
+		AddMapEntry(new Color(28, 216, 151));
+		DustType = DustID.Flare_Blue;
+	}
+
+	public override void Convert(int i, int j, int conversionType)
+	{
+		if (conversionType == BiomeConversionID.PurificationPowder)
+			WorldGen.ConvertTile(i, j, TileID.GolfGrass);
+	}
+
+	public override bool TileFrame(int i, int j, ref bool resetFrame, ref bool noBreak)
+	{
+		if (OpenTools.GetOpenings(i, j, false, false, true) == OpenFlags.None) //Surrounded by solid tiles
+			Main.tile[i, j].TileType = TileID.GolfGrass;
+
+		return true;
+	}
+
+	public override void GrowPlants(int i, int j) { }
+}
