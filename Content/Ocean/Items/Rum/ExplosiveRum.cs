ad6fe4ff20badddce7e4aeb7a1111ec778c75855
GabeHasWon <gabriel@guevarameyer.com>
2025-06-13 19:29:14 -0500

    fix my bad merge whoops

--- Content/Ocean/Items/Rum/ExplosiveRum.cs
+++ Content/Ocean/Items/Rum/ExplosiveRum.cs
@@ -12,7 +12,6 @@ public class ExplosiveRum : ModItem
 
 		NPCShopHelper.AddEntry(new NPCShopHelper.ConditionalEntry((shop) => shop.NpcType == NPCID.DD2Bartender,
 		  new NPCShop.Entry(ModContent.ItemType<ExplosiveRum>(), Condition.InBeach)));
-	}
 
 		MoRHelper.AddElement(Item, MoRHelper.Explosive);
 		MoRHelper.AddElement(Item, MoRHelper.Fire, true);
