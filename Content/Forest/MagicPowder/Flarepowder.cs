42c5da7c79650f0dcc7f852293bf533fdb057ea4
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2025-06-24 22:59:10 -0300

    Color fixes

--- Content/Forest/MagicPowder/Flarepowder.cs
+++ Content/Forest/MagicPowder/Flarepowder.cs
@@ -55,14 +55,12 @@ public class Flarepowder : ModItem
 	{
 		Item.ResearchUnlockCount = 99;
 
-		int frame = Name switch
+		PowderTypes.Add(Type, Name switch
 		{
 			nameof(VexpowderBlue) => 1,
 			nameof(VexpowderRed) => 2,
 			_ => 0
-		};
-
-		PowderTypes.Add(Type, frame);
+		});
 		
 		if (!IsDerived)
 		{
@@ -116,8 +114,9 @@ internal class FlarepowderDust : ModProjectile, IManualTrailProjectile
 {
 	public const int TimeLeftMax = 60 * 3;
 
-	/// <summary> Must have 3 elements. </summary>
-	public virtual Color[] Colors => [Color.Yellow, Color.Red, Color.DarkRed];
+	/// <summary> Must have 5 elements. Normally a gradient from light to dark. </summary>
+	public virtual Color[] Colors => [Color.LightGoldenrodYellow, Color.Yellow, Color.Orange, Color.OrangeRed, Color.DarkRed];
+
 	public override string Texture => DrawHelpers.RequestLocal(GetType(), nameof(FlarepowderDust));
 
 	public static readonly SoundStyle Impact = new("SpiritReforged/Assets/SFX/Projectile/Impact_Hard")
@@ -131,13 +130,13 @@ internal class FlarepowderDust : ModProjectile, IManualTrailProjectile
 	public (float, float) randomTimeLeft;
 	private bool _spawned = true;
 
-	public void DoTrailCreation(TrailManager tm)
+	public virtual void DoTrailCreation(TrailManager tm)
 	{
 		float scale = Projectile.scale;
 
-		tm.CreateTrail(Projectile, new StandardColorTrail(Color.OrangeRed.Additive()), new RoundCap(), new DefaultTrailPosition(), 10 * scale, 20 * scale);
+		tm.CreateTrail(Projectile, new StandardColorTrail(Colors[3].Additive()), new RoundCap(), new DefaultTrailPosition(), 10 * scale, 20 * scale);
 		tm.CreateTrail(Projectile, new LightColorTrail(new Color(130, 26, 12) * 0.6f, Color.Transparent), new RoundCap(), new DefaultTrailPosition(), 15 * scale, 50 * scale);
-		tm.CreateTrail(Projectile, new StandardColorTrail(Color.LightGoldenrodYellow.Additive()), new RoundCap(), new DefaultTrailPosition(), 5 * scale, 10 * scale);
+		tm.CreateTrail(Projectile, new StandardColorTrail(Colors[0].Additive()), new RoundCap(), new DefaultTrailPosition(), 5 * scale, 10 * scale);
 	}
 
 	public override void SetStaticDefaults() => Main.projFrames[Type] = 3;
@@ -183,13 +182,13 @@ internal class FlarepowderDust : ModProjectile, IManualTrailProjectile
 				var velocity = (Projectile.velocity * mag).RotatedByRandom(0.3f);
 
 				if (Main.rand.NextBool(3))
-					ParticleHandler.SpawnParticle(new MagicParticle(Projectile.Center, velocity * 0.75f, Color.OrangeRed, Main.rand.NextFloat(0.1f, 1f), Main.rand.Next(20, 100)));
+					ParticleHandler.SpawnParticle(new MagicParticle(Projectile.Center, velocity * 0.75f, Colors[3], Main.rand.NextFloat(0.1f, 1f), Main.rand.Next(20, 100)));
 
 				Vector2 cloudPos = Projectile.Center + Vector2.Normalize(Projectile.velocity) * 10;
-				var fireCloud = new SmokeCloud(cloudPos, velocity, Color.Yellow.Additive(80), Main.rand.NextFloat(0.05f, 0.075f), EaseFunction.EaseQuadOut, Main.rand.Next(20, 30), false)
+				var fireCloud = new SmokeCloud(cloudPos, velocity, Colors[1].Additive(80), Main.rand.NextFloat(0.05f, 0.075f), EaseFunction.EaseQuadOut, Main.rand.Next(20, 30), false)
 				{
-					SecondaryColor = Color.Red.Additive(80),
-					TertiaryColor = Color.DarkRed.Additive(80),
+					SecondaryColor = Color.Lerp(Colors[3], Colors[4], 0.5f).Additive(80),
+					TertiaryColor = Colors[4].Additive(80),
 					ColorLerpExponent = 0.5f,
 					Intensity = 0.25f,
 					Pixellate = true
@@ -233,58 +232,55 @@ internal class FlarepowderDust : ModProjectile, IManualTrailProjectile
 
 	public override void OnKill(int timeLeft)
 	{
-		if (!Main.dedServ)
-		{
-			float angle = Main.rand.NextFloat(MathHelper.Pi);
-
-			var circle = new TexturedPulseCircle(Projectile.Center, (Color.OrangeRed * .5f).Additive(), 2, 42, 20, "Bloom", new Vector2(1), Common.Easing.EaseFunction.EaseCircularOut);
-			circle.Angle = angle;
-			ParticleHandler.SpawnParticle(circle);
-
-			var circle2 = new TexturedPulseCircle(Projectile.Center, (Color.LightGoldenrodYellow * .5f).Additive(), 1, 40, 20, "Bloom", new Vector2(1), Common.Easing.EaseFunction.EaseCircularOut);
-			circle2.Angle = angle;
-			ParticleHandler.SpawnParticle(circle2);
-
-			if (this is not VexpowderBlueDust)
-			{
-				for (int i = 0; i < 3; i++)
-				{
-					Vector2 velocity = Main.rand.NextVector2Unit() * Main.rand.NextFloat() - Vector2.UnitY * Main.rand.NextFloat(2);
-					Color[] colors = [Colors[0].Additive(30), Colors[1].Additive(30), Colors[2].Additive(30) * 0.75f];
-					float scale = Main.rand.NextFloat(0.05f, 0.1f);
-					int maxTime = Main.rand.Next(15, 35);
-					ParticleHandler.SpawnParticle(new FireParticle(Projectile.Center, velocity, colors, 0.75f, scale, EaseFunction.EaseQuadOut, maxTime)
-					{
-						ColorLerpExponent = 2,
-						FinalScaleMod = 0.33f
-					});
-				}
-
-				var smokeCloud = new SmokeCloud(Projectile.Center, -Vector2.UnitY, Color.Gray, Main.rand.NextFloat(0.04f, 0.06f), EaseFunction.EaseCubicOut, Main.rand.Next(20, 40))
-				{
-					SecondaryColor = Color.DarkSlateGray,
-					TertiaryColor = Color.Black,
-					ColorLerpExponent = 2,
-					Intensity = 0.6f,
-					Layer = ParticleLayer.BelowProjectile,
-					Pixellate = true
-				};
-				ParticleHandler.SpawnParticle(smokeCloud);
-			}
-		}
-
 		const int explosion = 80;
 
 		Projectile.Resize(explosion, explosion);
 		Projectile.Damage();
 
-		PlayDeathSound();
+		if (!Main.dedServ)
+			DoDeathEffects();
 	}
 
-	public virtual void PlayDeathSound()
+	public virtual void DoDeathEffects()
 	{
 		SoundEngine.PlaySound(SoundID.DD2_LightningBugZap with { PitchRange = (0.5f, 1f), Volume = 0.35f, MaxInstances = 5 }, Projectile.Center);
 		SoundEngine.PlaySound(Impact, Projectile.Center);
+
+		float angle = Main.rand.NextFloat(MathHelper.Pi);
+
+		var circle = new TexturedPulseCircle(Projectile.Center, (Colors[3] * .5f).Additive(), 2, 42, 20, "Bloom", new Vector2(1), EaseFunction.EaseCircularOut);
+		circle.Angle = angle;
+		ParticleHandler.SpawnParticle(circle);
+
+		var circle2 = new TexturedPulseCircle(Projectile.Center, (Colors[0] * .5f).Additive(), 1, 40, 20, "Bloom", new Vector2(1), EaseFunction.EaseCircularOut);
+		circle2.Angle = angle;
+		ParticleHandler.SpawnParticle(circle2);
+
+		for (int i = 0; i < 3; i++)
+		{
+			Vector2 velocity = Main.rand.NextVector2Unit() * Main.rand.NextFloat() - Vector2.UnitY * Main.rand.NextFloat(2);
+			Color[] colors = [Colors[1].Additive(30), Colors[3].Additive(30), Colors[4].Additive(30) * 0.75f];
+
+			float scale = Main.rand.NextFloat(0.05f, 0.1f);
+			int maxTime = Main.rand.Next(15, 35);
+
+			ParticleHandler.SpawnParticle(new FireParticle(Projectile.Center, velocity, colors, 0.75f, scale, EaseFunction.EaseQuadOut, maxTime)
+			{
+				ColorLerpExponent = 2,
+				FinalScaleMod = 0.33f
+			});
+		}
+
+		var smokeCloud = new SmokeCloud(Projectile.Center, -Vector2.UnitY, Color.Gray, Main.rand.NextFloat(0.04f, 0.06f), EaseFunction.EaseCubicOut, Main.rand.Next(20, 40))
+		{
+			SecondaryColor = Color.DarkSlateGray,
+			TertiaryColor = Color.Black,
+			ColorLerpExponent = 2,
+			Intensity = 0.6f,
+			Layer = ParticleLayer.BelowProjectile,
+			Pixellate = true
+		};
+		ParticleHandler.SpawnParticle(smokeCloud);
 	}
 
 	public override bool PreDraw(ref Color lightColor)
@@ -294,7 +290,7 @@ internal class FlarepowderDust : ModProjectile, IManualTrailProjectile
 
 		for (int i = 0; i < 3; i++)
 		{
-			Color tint = (i == 2) ? Color.White : ((i == 1) ? Color.OrangeRed : Colors[0]);
+			Color tint = (i == 2) ? Color.White : ((i == 1) ? Colors[3] : Colors[1]);
 			Color color = Projectile.GetAlpha(tint).Additive();
 			float scale = Projectile.scale * (1f - i / 5f);
 
--- Content/Forest/MagicPowder/VexpowderBlue.cs
+++ Content/Forest/MagicPowder/VexpowderBlue.cs
@@ -33,7 +33,7 @@ public class VexpowderBlue : Flarepowder
 
 internal class VexpowderBlueDust : FlarepowderDust
 {
-	public override Color[] Colors => [Color.Violet, Color.BlueViolet, Color.DarkViolet];
+	public override Color[] Colors => [Color.LightGoldenrodYellow, Color.Violet, Color.BlueViolet, Color.Violet, Color.DarkViolet];
 
 	public override void OnClientSpawn(bool doDustSpawn)
 	{
@@ -48,23 +48,23 @@ internal class VexpowderBlueDust : FlarepowderDust
 			var velocity = (Projectile.velocity * mag).RotatedByRandom(0.2f);
 
 			if (Main.rand.NextBool())
-				ParticleHandler.SpawnParticle(new MagicParticle(Projectile.Center, velocity * 0.75f, Colors[0], Main.rand.NextFloat(0.1f, 1f), Main.rand.Next(20, 100)));
+				ParticleHandler.SpawnParticle(new MagicParticle(Projectile.Center, velocity * 0.75f, Colors[1], Main.rand.NextFloat(0.1f, 1f), Main.rand.Next(20, 100)));
 
 			Vector2 cloudPos = Projectile.Center + Vector2.Normalize(Projectile.velocity) * 10;
-			var fireCloud = new SmokeCloud(cloudPos, velocity, Colors[0].Additive(), Main.rand.NextFloat(0.08f, 0.12f), EaseFunction.EaseCubicOut, Main.rand.Next(40, 60), false)
+			var fireCloud = new SmokeCloud(cloudPos, velocity, Colors[1].Additive(), Main.rand.NextFloat(0.08f, 0.12f), EaseFunction.EaseCubicOut, Main.rand.Next(40, 60), false)
 			{
-				SecondaryColor = Colors[1].Additive(),
-				TertiaryColor = Colors[2].Additive(),
+				SecondaryColor = Colors[2].Additive(),
+				TertiaryColor = Colors[4].Additive(),
 				Intensity = 0.15f,
 				Pixellate = true
 			};
 
 			ParticleHandler.SpawnParticle(fireCloud);
 
-			var smokeCloud = new SmokeCloud(fireCloud.Position, fireCloud.Velocity * 1.25f, Color.Lerp(Color.Gray, Colors[0], 0.4f), fireCloud.Scale * 1.25f, EaseFunction.EaseCubicOut, Main.rand.Next(40, 70))
+			var smokeCloud = new SmokeCloud(fireCloud.Position, fireCloud.Velocity * 1.25f, Color.Lerp(Color.Gray, Colors[1], 0.4f), fireCloud.Scale * 1.25f, EaseFunction.EaseCubicOut, Main.rand.Next(40, 70))
 			{
-				SecondaryColor = Color.Lerp(Color.DarkSlateGray, Colors[1], 0.4f),
-				TertiaryColor = Color.Lerp(Color.Black, Colors[2], 0.4f),
+				SecondaryColor = Color.Lerp(Color.DarkSlateGray, Colors[2], 0.4f),
+				TertiaryColor = Color.Lerp(Color.Black, Colors[4], 0.4f),
 				ColorLerpExponent = 2,
 				Intensity = 0.33f,
 				Layer = ParticleLayer.BelowProjectile,
@@ -80,21 +80,26 @@ internal class VexpowderBlueDust : FlarepowderDust
 			target.AddBuff(BuffID.Confused, 150);
 	}
 
-	public override void OnKill(int timeLeft)
-	{
-		base.OnKill(timeLeft);
-
-		var lineScale = new Vector2(1f, 2f) * Projectile.scale * Main.rand.NextFloat(0.5f, 1.1f);
-		ParticleHandler.SpawnParticle(new ImpactLine(Projectile.Center, Vector2.Zero, Colors[0].Additive() * 0.5f, lineScale, 15));
-		ParticleHandler.SpawnParticle(new ImpactLine(Projectile.Center, Vector2.Zero, Color.White.Additive() * 0.5f, lineScale * 0.5f, 15));
-	}
-
 	public override void SpawnDust(Vector2 origin) => Dust.NewDustPerfect(origin, DustID.PurpleCrystalShard, Projectile.velocity * 0.5f).noGravity = true;
-	public override void PlayDeathSound()
+	public override void DoDeathEffects()
 	{
 		SoundEngine.PlaySound(SoundID.DD2_LightningBugZap with { PitchRange = (1f, 1.5f), Volume = 0.6f, MaxInstances = 5 }, Projectile.Center);
 		SoundEngine.PlaySound(SoundID.DD2_DarkMageHealImpact with { Pitch = 0.9f }, Projectile.Center);
 
 		SoundEngine.PlaySound(Impact with { Pitch = 0.9f, Volume = 0.4f }, Projectile.Center);
+
+		float angle = Main.rand.NextFloat(MathHelper.Pi);
+
+		var circle = new TexturedPulseCircle(Projectile.Center, (Colors[3] * .5f).Additive(), 2, 42, 20, "Bloom", new Vector2(1), EaseFunction.EaseCircularOut);
+		circle.Angle = angle;
+		ParticleHandler.SpawnParticle(circle);
+
+		var circle2 = new TexturedPulseCircle(Projectile.Center, (Colors[0] * .5f).Additive(), 1, 40, 20, "Bloom", new Vector2(1), EaseFunction.EaseCircularOut);
+		circle2.Angle = angle;
+		ParticleHandler.SpawnParticle(circle2);
+
+		var lineScale = new Vector2(1f, 2f) * Projectile.scale * Main.rand.NextFloat(0.5f, 1.1f);
+		ParticleHandler.SpawnParticle(new ImpactLine(Projectile.Center, Vector2.Zero, Colors[2].Additive() * 0.5f, lineScale, 15));
+		ParticleHandler.SpawnParticle(new ImpactLine(Projectile.Center, Vector2.Zero, Color.White.Additive() * 0.5f, lineScale * 0.5f, 15));
 	}
 }
