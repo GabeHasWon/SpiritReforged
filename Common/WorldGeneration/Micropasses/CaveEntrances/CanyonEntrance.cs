c6870761dcc6dc0c5b6c6b278959f43c81117149
GabeHasWon <gabriel@guevarameyer.com>
2025-06-27 13:04:34 -0500

    more canyon touchups

--- Common/WorldGeneration/Micropasses/CaveEntrances/CanyonEntrance.cs
+++ Common/WorldGeneration/Micropasses/CaveEntrances/CanyonEntrance.cs
@@ -29,7 +29,9 @@ internal class CanyonEntrance : CaveEntrance
 
 	private static bool CreateMound(int x, int y, int depth)
 	{
-		QuickConversion.BiomeType biome = QuickConversion.FindConversionBiome(new Point16(x - 40, y - 40), new Point16(80, 80));
+		Dictionary<QuickConversion.BiomeType, float> biases = new() { { QuickConversion.BiomeType.Purity, 0.7f }, { QuickConversion.BiomeType.Jungle, 4 }, 
+			{ QuickConversion.BiomeType.Desert, 4 } };
+		QuickConversion.BiomeType biome = QuickConversion.FindConversionBiome(new Point16(x - 40, y - 40), new Point16(80, 80), biases);
 
 		if (biome == QuickConversion.BiomeType.Desert)
 			return true;
@@ -43,7 +45,7 @@ internal class CanyonEntrance : CaveEntrance
 			_ => TileID.Dirt,
 		};
 
-		var mound = new Shapes.Mound(WorldGen.genRand.Next(26, 36), depth);
+		var mound = new Shapes.Mound(WorldGen.genRand.Next(46, 56), depth);
 		WorldUtils.Gen(new Point(x, y + depth), mound, Actions.Chain(new Modifiers.Blotches(), new Actions.PlaceTile(type)));
 		return false;
 	}
@@ -51,15 +53,20 @@ internal class CanyonEntrance : CaveEntrance
 	public static void DigCavern(int x, int y, int depth)
 	{
 		int tileY = WorldMethods.FindGround(x, y);
-		QuickConversion.BiomeType biome = QuickConversion.FindConversionBiome(new Point16(x - 40, tileY - 40), new Point16(80, 80));
+		Dictionary<QuickConversion.BiomeType, float> biases = new() { { QuickConversion.BiomeType.Purity, 0.7f }, { QuickConversion.BiomeType.Jungle, 4 },
+			{ QuickConversion.BiomeType.Desert, 4 } };
+		QuickConversion.BiomeType biome = QuickConversion.FindConversionBiome(new Point16(x - 40, tileY - 40), new Point16(80, 80),	biases);
+
+		if (biome == QuickConversion.BiomeType.Desert)
+			return;
 
 		(ushort wallDirt, ushort wallStone) = biome switch
 		{
-			QuickConversion.BiomeType.Jungle => (WallID.MudUnsafe, WallID.JungleUnsafe),
+			QuickConversion.BiomeType.Jungle => (WallID.MudUnsafe, WallID.JungleUnsafe2),
 			QuickConversion.BiomeType.Ice => (WallID.SnowWallUnsafe, WallID.IceUnsafe),
 			QuickConversion.BiomeType.Corruption => (WallID.CorruptGrassUnsafe, WallID.DirtUnsafe1),
 			QuickConversion.BiomeType.Crimson => (WallID.CrimsonGrassUnsafe, WallID.DirtUnsafe1),
-			_ => (WallID.DirtUnsafe, WallID.GrassUnsafe),
+			_ => (WallID.GrassUnsafe, WallID.FlowerUnsafe),
 		};
 
 		FastNoiseLite diggingNoise = new(WorldGen._genRandSeed);
@@ -69,7 +76,7 @@ internal class CanyonEntrance : CaveEntrance
 		windingNoise.SetFrequency(0.007f);
 
 		FastNoiseLite wallNoise = new(WorldGen._genRandSeed);
-		wallNoise.SetFrequency(0.04f);
+		wallNoise.SetFrequency(0.03f);
 
 		for (int j = y; j < y + depth; j++)
 		{
@@ -77,9 +84,6 @@ internal class CanyonEntrance : CaveEntrance
 			int minDistance = (int)MathHelper.Lerp(8, 1, (j - y) / (float)depth);
 			int leftEdge = Math.Max(2, (int)(diggingNoise.GetNoise(x + 1200, j) * 4) + minDistance);
 			int rightEdge = Math.Max(2, (int)(diggingNoise.GetNoise(x + 2400, j) * 4) + minDistance);
-
-			int wallLeftEdge = (int)(wallNoise.GetNoise(x + 1200, j + 400) * 12) + minDistance;
-			int wallRightEdge = (int)(wallNoise.GetNoise(x + 2400, j + 400) * 12) + minDistance;
 
 			int left = useX - leftEdge * 2;
 			int right = useX + rightEdge * 2;
@@ -95,16 +99,14 @@ internal class CanyonEntrance : CaveEntrance
 				if (canClear && withinTiles)
 					tile.Clear(TileDataType.Tile);
 
-				if (i > useX - wallLeftEdge && i < useX + wallRightEdge)
-					tile.Clear(TileDataType.Wall);
-				else if ((tile.HasTile || withinTiles) && j > y + 15)
+				if ((tile.HasTile && !WorldGen.TileIsExposedToAir(i, j) || withinTiles) && j > y + 8 + wallNoise.GetNoise(i, j) * 6)
 				{
 					float noise = wallNoise.GetNoise(i, j);
 
-					if (noise < 0.16f)
-						tile.WallType = wallStone;
-					else
+					if (noise < 0.3f)
 						tile.WallType = wallDirt;
+					else
+						tile.WallType = wallStone;
 				}
 			}
 		}
