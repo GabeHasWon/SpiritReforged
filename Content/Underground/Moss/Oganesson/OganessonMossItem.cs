d8e7c33ca02271d0f790bde4b1274156cb98e552
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2025-07-01 18:54:10 -0300

    Moss item tweaks

--- Content/Underground/Moss/Oganesson/OganessonMossItem.cs
+++ Content/Underground/Moss/Oganesson/OganessonMossItem.cs
@@ -25,6 +25,7 @@ public class OganessonMossItem : ModItem
 		Item.autoReuse = true;
 		Item.consumable = true;
 		Item.rare = ItemRarityID.Blue;
+		Item.createTile = ModContent.TileType<OganessonMoss>();
 	}
 
 	public override void HoldItem(Player player)
@@ -36,35 +37,46 @@ public class OganessonMossItem : ModItem
 		}
 	}
 
-	public override bool CanUseItem(Player player) => player.IsTargetTileInItemRange(Item);
+	public override bool CanUseItem(Player player)
+	{
+		if (player.whoAmI == Main.myPlayer)
+		{
+			Item.createTile = GetPlaceType(player.TargetTile().TileType);
+			if (Item.createTile != -1 && player.IsTargetTileInItemRange(Item))
+			{
+				int i = Player.tileTargetX;
+				int j = Player.tileTargetY;
+
+				Main.tile[i, j].TileType = (ushort)Item.createTile;
+				WorldGen.Reframe(i, j);
+
+				if (Main.netMode != NetmodeID.SinglePlayer)
+					NetMessage.SendTileSquare(-1, i, j);
+			}
+		}
+		else
+		{
+			Item.createTile = -1;
+		}
+
+		return true;
+	}
+
+	/// <param name="tileType"> The target tile type. </param>
+	public virtual int GetPlaceType(int tileType) => tileType switch
+	{
+		TileID.GrayBrick => ModContent.TileType<OganessonMossGrayBrick>(),
+		TileID.Stone => ModContent.TileType<OganessonMoss>(),
+		_ => -1
+	};
 
 	public override bool? UseItem(Player player)
 	{
-		var tile = player.TargetTile();
-		if (tile.TileType == TileID.Stone)
-		{
-			TryPlace(ModContent.TileType<OganessonMoss>());
-			return true;
-		}
-		else if (tile.TileType == TileID.GrayBrick)
-		{
-			TryPlace(ModContent.TileType<OganessonMossGrayBrick>());
-			return true;
-		}
-
+		Item.createTile = ModContent.TileType<OganessonMoss>();
 		return null;
-
-		static void TryPlace(int type)
-		{
-			WorldGen.PlaceTile(Player.tileTargetX, Player.tileTargetY, type, forced: true);
-			var t = Main.tile[Player.tileTargetX, Player.tileTargetY];
-
-			if (t.TileType == type && Main.netMode != NetmodeID.SinglePlayer)
-				NetMessage.SendTileSquare(-1, Player.tileTargetX, Player.tileTargetY);
-		}
 	}
 
-	public override void Update(ref float gravity, ref float maxFallSpeed) => Lighting.AddLight(Item.position, .252f, .252f, .252f);
+	public override void Update(ref float gravity, ref float maxFallSpeed) => Lighting.AddLight(Item.position, 0.252f, 0.252f, 0.252f);
 	public override bool PreDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, ref float rotation, ref float scale, int whoAmI)
 	{
 		Item.DrawInWorld(Color.White, rotation, scale);
