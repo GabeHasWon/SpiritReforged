0e6455a317475484c9bb374d66b1cc8f8a94d6a3
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2025-07-20 23:20:55 -0300

    Add ambient sounds

---
+++ Common/NPCCommon/Abstract/Miniboss.cs
@@ -0,0 +1,16 @@
+﻿using Terraria.Audio;
+
+namespace SpiritReforged.Common.NPCCommon.Abstract;
+
+public abstract class Miniboss : ModNPC
+{
+	public static readonly SoundStyle Downed = new("SpiritReforged/Assets/SFX/NPCDeath/DownedMiniboss");
+
+	/// <summary><inheritdoc cref="ModNPC.HitEffect"/><para/>
+	/// Plays the miniboss death sound effect by default. </summary>
+	public override void HitEffect(NPC.HitInfo hit)
+	{
+		if (NPC.life <= 0 && !Main.dedServ)
+			SoundEngine.PlaySound(Downed, NPC.Center);
+	}
+}
