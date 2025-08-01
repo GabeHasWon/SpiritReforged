bb074f668ee542304ae3b52cd3a9f841f9588d6c
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2025-08-01 18:50:34 -0300

    Visual tweaks

--- Content/Desert/Silk/AfterimagePlayer.cs
+++ Content/Desert/Silk/AfterimagePlayer.cs
@@ -1,6 +1,8 @@
-﻿using SpiritReforged.Common.Misc;
+﻿using SpiritReforged.Common.Easing;
+using SpiritReforged.Common.Misc;
 using SpiritReforged.Common.Particle;
 using SpiritReforged.Content.Particles;
+using SpiritReforged.Content.Underground.Items.BigBombs;
 using SpiritReforged.Content.Underground.WayfarerSet;
 using System.Runtime.CompilerServices;
 using Terraria.Audio;
@@ -39,14 +41,17 @@ public class AfterimagePlayer : ModPlayer
 	{
 		sb.Begin(SpriteSortMode.Immediate, BlendState.Additive, camera.Sampler, DepthStencilState.None, camera.Rasterizer, null, Main.GameViewMatrix.TransformationMatrix);
 
-		Lighting.AddLight(position, new Vector3(0.5f, 0.3f, 0.05f));
-		float scale = 1f + MathHelper.Clamp(1f - drawPlayer.position.DistanceSQ(position) / 100, 0, 1) * 0.05f;
+		var afterimager = drawPlayer.GetModPlayer<AfterimagePlayer>();
+		float manaStrength = (afterimager._duplicateDelay > 0) ? 1 : ((float)afterimager._manaCounter / ManaThreshold);
+		float mult = MathHelper.Clamp(drawPlayer.position.DistanceSQ(position) / 1000f, 0, 1) * manaStrength;
+
+		Lighting.AddLight(position, new Vector3(0.5f, 0.3f, 0.05f) * mult);
 
 		for (int a = 0; a < TrailLength; a++)
-			self.DrawPlayer(camera, drawPlayer, drawPlayer.GetModPlayer<AfterimagePlayer>()._positionCache[29 - a], drawPlayer.fullRotation, drawPlayer.fullRotationOrigin, 1f - a / (float)TrailLength, scale);
+			self.DrawPlayer(camera, drawPlayer, afterimager._positionCache[29 - a], drawPlayer.fullRotation, drawPlayer.fullRotationOrigin, 1f - a / (float)TrailLength, 1);
 
 		for (int i = 0; i < 3; i++)
-			self.DrawPlayer(camera, drawPlayer, position, drawPlayer.fullRotation, drawPlayer.fullRotationOrigin, 0, scale);
+			self.DrawPlayer(camera, drawPlayer, position, drawPlayer.fullRotation, drawPlayer.fullRotationOrigin, 1f - mult, 1);
 
 		var glow = TextureAssets.Extra[98].Value;
 		var bloom = AssetLoader.LoadedTextures["Bloom"].Value;
@@ -55,16 +60,16 @@ public class AfterimagePlayer : ModPlayer
 		var glowScale = new Vector2(0.5f + wave * 0.4f, 1 - wave * 0.4f) * 1.2f;
 		var glowPosition = position + drawPlayer.Size / 2 - new Vector2(0, 12).RotatedBy(drawPlayer.fullRotation) - Main.screenPosition;
 
-		sb.Draw(bloom, glowPosition, null, Color.Orange, 0, bloom.Size() / 2, 0.3f, default, 0);
+		sb.Draw(bloom, glowPosition, null, Color.Orange * mult, 0, bloom.Size() / 2, 0.3f, default, 0);
 
-		sb.Draw(glow, glowPosition, null, Color.Orange * 0.5f, 0, glow.Size() / 2, glowScale * 1.3f, default, 0);
-		sb.Draw(glow, glowPosition, null, Color.Goldenrod, 0, glow.Size() / 2, glowScale, default, 0);
-		sb.Draw(glow, glowPosition, null, Color.White, 0, glow.Size() / 2, glowScale * 0.7f, default, 0);
+		sb.Draw(glow, glowPosition, null, Color.Orange * 0.5f * mult, 0, glow.Size() / 2, glowScale * 1.3f, default, 0);
+		sb.Draw(glow, glowPosition, null, Color.Goldenrod * mult, 0, glow.Size() / 2, glowScale, default, 0);
+		sb.Draw(glow, glowPosition, null, Color.White * mult, 0, glow.Size() / 2, glowScale * 0.7f, default, 0);
 
 		float angled = MathHelper.PiOver2;
-		sb.Draw(glow, glowPosition, null, Color.Orange * 0.5f, angled, glow.Size() / 2, glowScale * 1.3f, default, 0);
-		sb.Draw(glow, glowPosition, null, Color.Goldenrod, angled, glow.Size() / 2, glowScale, default, 0);
-		sb.Draw(glow, glowPosition, null, Color.White, angled, glow.Size() / 2, glowScale * 0.7f, default, 0);
+		sb.Draw(glow, glowPosition, null, Color.Orange * 0.5f * mult, angled, glow.Size() / 2, glowScale * 1.3f, default, 0);
+		sb.Draw(glow, glowPosition, null, Color.Goldenrod * mult, angled, glow.Size() / 2, glowScale, default, 0);
+		sb.Draw(glow, glowPosition, null, Color.White * mult, angled, glow.Size() / 2, glowScale * 0.7f, default, 0);
 
 		sb.End();
 	}
@@ -75,13 +80,15 @@ public class AfterimagePlayer : ModPlayer
 		{
 			if (Main.rand.NextBool(5) && Player.position != ImagePosition)
 			{
+				float manaStrength = (_duplicateDelay > 0) ? 1 : ((float)_manaCounter / ManaThreshold);
 				float strength = Main.rand.NextFloat();
 				var position = Main.rand.NextVector2FromRectangle(new((int)ImagePosition.X, (int)ImagePosition.Y, Player.width, Player.height));
 				var velocity = ImagePosition.DirectionTo(Player.position) * strength * 3;
 
-				ParticleHandler.SpawnParticle(new EmberParticle(position, velocity, Color.Lerp(Color.OrangeRed, Color.Yellow, strength).Additive(), MathHelper.Lerp(0.5f, 2, strength), 20, 1) { emitLight = false });
+				ParticleHandler.SpawnParticle(new EmberParticle(position, velocity, Color.Lerp(Color.OrangeRed, Color.Yellow, strength).Additive(), MathHelper.Lerp(0.5f, 2, strength) * manaStrength, 20, 1) { emitLight = false });
 			}
 
+			//Update old positions
 			for (int i = _positionCache.Length - 1; i > 0; i--)
 				_positionCache[i] = _positionCache[i - 1];
 
@@ -101,6 +108,19 @@ public class AfterimagePlayer : ModPlayer
 
 			Vector2 lineScale = new(0.5f, 1);
 			var glowPos = ImagePosition + Player.Size / 2 - new Vector2(0, 12).RotatedBy(Player.fullRotation);
+			var ease = Bomb.EffectEase;
+			var stretch = Vector2.One;
+			float angle = Main.rand.NextFloat(-MathHelper.TwoPi, MathHelper.TwoPi);
+
+			ParticleHandler.SpawnParticle(new TexturedPulseCircle(glowPos, Color.Goldenrod.Additive(), Color.OrangeRed.Additive(), 1f, 180, 20, "Smoke", stretch, ease)
+			{
+				Angle = angle
+			});
+
+			ParticleHandler.SpawnParticle(new TexturedPulseCircle(glowPos, Color.White.Additive(), Color.OrangeRed.Additive(), .5f, 180, 20, "Smoke", stretch, ease)
+			{
+				Angle = angle
+			});
 
 			for (int i = 0; i < 5; i++)
 			{
