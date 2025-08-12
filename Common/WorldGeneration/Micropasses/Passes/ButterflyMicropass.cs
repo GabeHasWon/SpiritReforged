aef9b744f7fe18e440e6c6d6c2dde6747405bf9d
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2025-08-12 10:58:02 -0300

    ButterflyMicropass cleanup

--- Common/WorldGeneration/Micropasses/Passes/ButterflyMicropass.cs
+++ Common/WorldGeneration/Micropasses/Passes/ButterflyMicropass.cs
@@ -16,51 +16,35 @@ internal class ButterflyMicropass : Micropass
 
 	public override void Run(GenerationProgress progress, Terraria.IO.GameConfiguration config)
 	{
-		const int maxTries = 2000;
+		const int maxAttempts = 2000;
 
 		progress.Message = Language.GetTextValue("Mods.SpiritReforged.Generation.Butterfly");
-		int repeats = Main.maxTilesX / WorldGen.WorldSizeSmallX; // 1 shrine in small and medium worlds, 2 in large
+		int count = 0;
+		int maxCount = Main.maxTilesX / WorldGen.WorldSizeSmallX; // 1 shrine in small and medium worlds, 2 in large
 
-		for (int i = 0; i < repeats; i++)
+		Point16 size = ButterflyShrineBiome.Size;
+		int third = Main.maxTilesX / 3;
+
+		for (int a = 0; a < maxAttempts; a++)
 		{
-			Point16 size = ButterflyShrineBiome.Size; 
-			Point16 position = Point16.Zero;
+			Point16 pt = new(
+				WorldGen.genRand.NextBool() ? WorldGen.genRand.Next(GenVars.leftBeachEnd, third) : WorldGen.genRand.Next(Main.maxTilesX - third, GenVars.rightBeachStart),
+				(int)GenVars.worldSurface + WorldGen.genRand.Next(50, 100));
 
-			int tries = 0;
-			while (tries < maxTries)
-			{
-				int third = Main.maxTilesX / 3;
-				int x = WorldGen.genRand.NextBool() ? WorldGen.genRand.Next(GenVars.leftBeachEnd, third) : WorldGen.genRand.Next(Main.maxTilesX - third, GenVars.rightBeachStart);
-				int y = (int)GenVars.worldSurface + WorldGen.genRand.Next(50, 100);
+			Dictionary<ushort, int> typeToCount = [];
+			WorldUtils.Gen(new Point(pt.X, pt.Y) - new Point(size.X / 2, size.Y / 2), new Shapes.Rectangle(size.X, size.Y), new Actions.TileScanner(TileID.Dirt).Output(typeToCount));
 
-				position = new Point16(x, y);
-
-				if (SolidPerimeter(new Rectangle(position.X, position.Y, size.X, size.Y)))
-					break;
-
-				tries++;
-			}
-
-			if (tries == maxTries)
-			{
-				SpiritReforgedMod.Instance.Logger.Info("Generator exceeded maximum tries for structure: Butterfly Shrine");
-				return; //Failed
-			}
-
-			if (GenVars.structures.CanPlace(new Rectangle(position.X, position.Y, size.X, size.Y), 4))
+			if (typeToCount[TileID.Dirt] > size.X * size.Y * 0.5f && GenVars.structures.CanPlace(new Rectangle(pt.X, pt.Y, size.X, size.Y), 4))
 			{
 				var blacklist = new QuickConversion.BiomeType[] { QuickConversion.BiomeType.Jungle, QuickConversion.BiomeType.Mushroom, QuickConversion.BiomeType.Desert, QuickConversion.BiomeType.Ice };
-				var biome = QuickConversion.FindConversionBiome(position, size);
-	
+				var biome = QuickConversion.FindConversionBiome(pt, size);
+
 				if (blacklist.Contains(biome))
-				{
-					i--;
 					continue;
-				}
 
-				Microbiome.Create<ButterflyShrineBiome>(new(position.X + size.X / 2, position.Y));
+				Microbiome.Create<ButterflyShrineBiome>(new(pt.X + size.X / 2, pt.Y));
 
-				var origin = new Point(position.X + size.X / 2, position.Y + 8); //Centered position
+				var origin = new Point(pt.X + size.X / 2, pt.Y + 8); //Centered position
 				bool foundClearing = WorldUtils.Find(origin, Searches.Chain(new Searches.Up(1000), new Conditions.IsSolid().AreaOr(1, 50).Not()), out var top);
 				top.Y += 50;
 
@@ -78,60 +62,22 @@ internal class ButterflyMicropass : Micropass
 					));
 
 					WorldUtils.Gen(shaftOrigin, new Shapes.Rectangle(1, shaftHeight), Actions.Chain(
-						new Modifiers.Blotches(2, 0.2), 
-						new Modifiers.SkipTiles(Ignore), 
-						new Actions.ClearTile().Output(data), 
-						new Modifiers.Expand(1), 
-						new Modifiers.OnlyTiles(TileID.Sand), 
+						new Modifiers.Blotches(2, 0.2),
+						new Modifiers.SkipTiles(Ignore),
+						new Actions.ClearTile().Output(data),
+						new Modifiers.Expand(1),
+						new Modifiers.OnlyTiles(TileID.Sand),
 						new Actions.SetTileKeepWall(TileID.HardenedSand).Output(data)
 					));
 
 					WorldUtils.Gen(new Point(origin.X, top.Y + 10), new ModShapes.All(data), new Actions.SetFrames(frameNeighbors: true));
 				}
-			}
-			else
-			{
-				i--;
-			}
-		}
-	}
 
-	static bool SolidPerimeter(Rectangle area) //Scan the perimeter of 'area' for solid and dirt tiles
-	{
-		const float solidMargin = .9f, dirtMargin = .5f; //at least 90% solid and 50% dirt
-		int solidCount = 0, dirtCount = 0;
-
-		for (int a = 0; a < 2; a++)
-		{
-			for (int x = area.X; x < area.X + area.Width; x++)
-			{
-				int y = area.Y + area.Height * a;
-				if (WorldGen.SolidTile(x, y))
-				{
-					solidCount++;
-
-					if (Main.tile[x, y].TileType == TileID.Dirt)
-						dirtCount++;
-				}
+				if (++count >= maxCount)
+					return;
 			}
 		}
 
-		for (int a = 0; a < 2; a++)
-		{
-			for (int y = area.Y; y < area.Y + area.Height; y++)
-			{
-				int x = area.X + area.Width * a;
-				if (WorldGen.SolidTile(x, y))
-				{
-					solidCount++;
-
-					if (Main.tile[x, y].TileType == TileID.Dirt)
-						dirtCount++;
-				}
-			}
-		}
-
-		int totalCount = area.Width * 2 + area.Height * 2;
-		return solidCount / (float)totalCount >= solidMargin && dirtCount / (float)totalCount >= dirtMargin;
+		SpiritReforgedMod.Instance.Logger.Info("Generator exceeded maximum tries for structure: Butterfly Shrine");
 	}
 }
