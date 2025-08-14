b09f38b4fc15f2ee4f12e4fc21f5a5a6dfe84a13
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2025-08-14 19:36:29 -0300

    Oasis parallax background

--- Content/Desert/Oasis/UndergroundOasisScene.cs
+++ Content/Desert/Oasis/UndergroundOasisScene.cs
@@ -1,4 +1,5 @@
-﻿using SpiritReforged.Common.Visuals;
+﻿using SpiritReforged.Common.TileCommon;
+using SpiritReforged.Common.Visuals;
 using SpiritReforged.Common.WorldGeneration.Microbiomes.Biomes;
 using Terraria.Graphics.Effects;
 
@@ -10,6 +11,7 @@ public class UndergroundOasisScene : ModSceneEffect
 
 	public override SceneEffectPriority Priority => SceneEffectPriority.BiomeLow;
 	public override int Music => MusicID.Desert;
+	public override ModUndergroundBackgroundStyle UndergroundBackgroundStyle => ModContent.GetInstance<UndergroundOasisBackground>();
 
 	public override bool IsSceneEffectActive(Player player) => UndergroundOasisBiome.InUndergroundOasis(player);
 	public override void SpecialVisuals(Player player, bool isActive)
@@ -39,13 +41,61 @@ public class UndergroundOasisScene : ModSceneEffect
 
 public class UndergroundOasisBackground : ModUndergroundBackgroundStyle, IWaterStyle
 {
-	public override void FillTextureArray(int[] textureSlots)
+	public static readonly Asset<Texture2D> OpenBackground1 = ModContent.Request<Texture2D>("SpiritReforged/Assets/Textures/Backgrounds/OasisBackground1");
+	public static readonly Asset<Texture2D> OpenBackground2 = ModContent.Request<Texture2D>("SpiritReforged/Assets/Textures/Backgrounds/OasisBackground2");
+
+	public override void Load() => On_Main.DrawBackground += DrawOpenBackground;
+	private static void DrawOpenBackground(On_Main.orig_DrawBackground orig, Main self)
 	{
-		/*textureSlots[0] = 0;
-		textureSlots[1] = 1;
-		textureSlots[2] = 2;
-		textureSlots[3] = 3;*/
+		//const int roundWidth = 128; //160 - 32
+		//const int roundHeight = 96;
+		const float scrollSpeed = 0.137f;
+
+		orig(self);
+
+		if (Main.LocalPlayer.Center.Y / 16 > Main.worldSurface && Main.LocalPlayer.ZoneDesert)
+		{
+			var sb = Main.spriteBatch;
+			var background = OpenBackground1.Value;
+			var subBackground = OpenBackground2.Value;
+
+			foreach (var area in UndergroundOasisBiome.OasisAreas)
+			{
+				var worldCoords = area.Center.ToWorldCoordinates();
+				var roundedPos = worldCoords; //new Vector2((int)(worldCoords.X / roundWidth) * roundWidth, (int)(worldCoords.Y / roundHeight) * roundHeight);
+
+				Vector2 scroll = new((Main.screenPosition.X - roundedPos.X) * Main.caveParallax * scrollSpeed, 0);
+				Vector2 instantParallax = new(MathHelper.Clamp((Main.LocalPlayer.Center.X - roundedPos.X) * 0.03f, -16, 16), 0);
+
+				var center = roundedPos + scroll + new Vector2(120, -100);
+
+				sb.Draw(TextureAssets.MagicPixel.Value, center + instantParallax - Main.screenPosition + TileExtensions.TileOffset, subBackground.Bounds, new Color(0.02f, 0.025f, 0.025f), 0, subBackground.Size() / 2, 1, default, 0);
+
+				DrawBackgroundSliced(sb, subBackground, center + instantParallax, Color.White * 0.7f);
+				DrawBackgroundSliced(sb, background, center);
+			}
+		}
+
+		static void DrawBackgroundSliced(SpriteBatch sb, Texture2D texture, Vector2 topLeft, Color? tint = null)
+		{
+			const int sliceScale = 8;
+			topLeft -= texture.Size() / 2;
+
+			for (int x = 0; x < texture.Width / sliceScale; x++)
+			{
+				for (int y = 0; y < texture.Height / sliceScale; y++)
+				{
+					var offset = new Vector2(x, y) * sliceScale;
+					Vector3 light = Lighting.GetSubLight(topLeft + offset) * 0.9f;
+					Rectangle source = new((int)offset.X, (int)offset.Y, sliceScale, sliceScale);
+
+					sb.Draw(texture, topLeft + offset - Main.screenPosition + TileExtensions.TileOffset, source, (tint is Color finalTint) ? new Color(light).MultiplyRGB(finalTint) : new Color(light));
+				}
+			}
+		}
 	}
+
+	public override void FillTextureArray(int[] textureSlots) => ModContent.GetInstance<UndergroundDesertBackground>().FillTextureArray(textureSlots);
 
 	public void ForceWaterStyle(ref int style)
 	{
