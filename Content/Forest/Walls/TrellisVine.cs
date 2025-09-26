6dc2ada9b7add2f9c1dacbb2d08d8a349d0f2358
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2025-09-26 13:32:36 -0300

    Trellis vines

---
+++ Content/Forest/Walls/TrellisVine.cs
@@ -0,0 +1,50 @@
+﻿using SpiritReforged.Common.TileCommon;
+using Terraria.DataStructures;
+
+namespace SpiritReforged.Content.Forest.Walls;
+
+public class TrellisVine : ModTile
+{
+	public override void SetStaticDefaults()
+	{
+		Main.tileNoFail[Type] = true;
+		Main.tileSolid[Type] = false;
+		Main.tileMergeDirt[Type] = false;
+		Main.tileBlockLight[Type] = false;
+		Main.tileFrameImportant[Type] = true;
+
+		TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3Wall);
+		TileObjectData.newTile.Width = TileObjectData.newTile.Height = 1;
+		TileObjectData.newTile.Origin = Point16.Zero;
+		TileObjectData.newTile.CoordinateWidth = 24;
+		TileObjectData.newTile.CoordinateHeights = [22];
+		TileObjectData.newTile.RandomStyleRange = 5;
+		TileObjectData.addTile(Type);
+
+		AddMapEntry(new Color(90, 165, 100));
+		HitSound = SoundID.Grass with { Pitch = 0.2f, PitchVariance = 0.1f };
+		DustType = DustID.Grass;
+	}
+
+	public override bool PreDraw(int i, int j, SpriteBatch spriteBatch)
+	{
+		if (TileExtensions.GetVisualInfo(i, j, out Color color, out Texture2D texture))
+		{
+			Tile tile = Main.tile[i, j];
+			var data = TileObjectData.GetTileData(tile);
+			var source = new Rectangle(tile.TileFrameX, tile.TileFrameY, data.CoordinateWidth, data.CoordinateFullHeight);
+			var position = new Vector2(i, j).ToWorldCoordinates(8, 0) - Main.screenPosition + TileExtensions.TileOffset;
+			var origin = new Vector2(source.Width / 2, 4);
+
+			if (Framing.GetTileSafely(i + 1, j + 1).TileType == Type)
+			{
+				const float lightness = 0.6f;
+				spriteBatch.Draw(texture, position + new Vector2(8), source, color.MultiplyRGB(new Color(lightness, lightness, lightness)), 0, origin, 1, SpriteEffects.None, 0);
+			}
+
+			spriteBatch.Draw(texture, position, source, color, 0, origin, 1, SpriteEffects.None, 0);
+		}
+
+		return false;
+	}
+}
--- Content/Forest/Walls/Trellises.cs
+++ Content/Forest/Walls/Trellises.cs
@@ -1,9 +1,10 @@
 ﻿using SpiritReforged.Common.ItemCommon;
+using SpiritReforged.Common.TileCommon;
 using SpiritReforged.Common.WallCommon;
 
 namespace SpiritReforged.Content.Forest.Walls;
 
-public class Trellis : ModWall, IAutoloadWallItem
+public class Trellis : ModWall, IAutoloadWallItem, ICheckItemUse
 {
 	public virtual void AddItemRecipes(ModItem item)
 	{
@@ -15,137 +16,118 @@ public class Trellis : ModWall, IAutoloadWallItem
 	{
 		Main.wallHouse[Type] = true;
 		Main.wallLight[Type] = true;
-		DustType = DustID.WoodFurniture;
 
-		var entryColor = new Color(72, 50, 35);
-		AddMapEntry(entryColor);
+		DustType = DustID.WoodFurniture;
+		AddEntry();
+	}
+
+	public virtual void AddEntry() => AddMapEntry(new Color(72, 50, 35));
+
+	public override void RandomUpdate(int i, int j)
+	{
+		if (Main.rand.NextBool(9) && WorldGen.CountNearBlocksTypes(i, j, 1, 1, ModContent.TileType<TrellisVine>()) == 1)
+			Placer.PlaceTile(i, j, Type).Send();
 	}
 
 	public override void NumDust(int i, int j, bool fail, ref int num) => num = fail ? 1 : 3;
+	public bool? CheckItemUse(int type, int i, int j)
+	{
+		if (type == ItemID.GrassSeeds)
+		{
+			Placer.PlaceTile<TrellisVine>(i, j).Send();
+			return true;
+		}
+
+		return null;
+	}
 }
 
-public class TrellisBoreal : ModWall, IAutoloadWallItem
+public class TrellisBoreal : Trellis
 {
-	public virtual void AddItemRecipes(ModItem item)
+	public override void AddItemRecipes(ModItem item)
 	{
 		item.CreateRecipe(4).AddIngredient(ItemID.BorealWood).AddTile(TileID.Sawmill).Register();
 		item.CreateRecipe(4).AddIngredient(AutoContent.ItemType<TrellisTwoBoreal>(), 4).AddTile(TileID.Sawmill).Register();
 	}
 
-	public override void SetStaticDefaults()
+	public override void AddEntry()
 	{
-		Main.wallHouse[Type] = true;
-		Main.wallLight[Type] = true;
 		DustType = DustID.BorealWood;
-
-		var entryColor = new Color(72, 60, 50);
-		AddMapEntry(entryColor);
+		AddMapEntry(new Color(72, 60, 50));
 	}
-
-	public override void NumDust(int i, int j, bool fail, ref int num) => num = fail ? 1 : 3;
 }
 
-public class TrellisEbonwood : ModWall, IAutoloadWallItem
+public class TrellisEbonwood : Trellis
 {
-	public virtual void AddItemRecipes(ModItem item)
+	public override void AddItemRecipes(ModItem item)
 	{
 		item.CreateRecipe(4).AddIngredient(ItemID.Ebonwood).AddTile(TileID.Sawmill).Register();
 		item.CreateRecipe(4).AddIngredient(AutoContent.ItemType<TrellisTwoEbonwood>(), 4).AddTile(TileID.Sawmill).Register();
 	}
 
-	public override void SetStaticDefaults()
+	public override void AddEntry()
 	{
-		Main.wallHouse[Type] = true;
-		Main.wallLight[Type] = true;
 		DustType = DustID.Ebonwood;
-
-		var entryColor = new Color(52, 50, 62);
-		AddMapEntry(entryColor);
+		AddMapEntry(new Color(52, 50, 62));
 	}
-
-	public override void NumDust(int i, int j, bool fail, ref int num) => num = fail ? 1 : 3;
 }
 
-public class TrellisMahogany : ModWall, IAutoloadWallItem
+public class TrellisMahogany : Trellis
 {
-	public virtual void AddItemRecipes(ModItem item)
+	public override void AddItemRecipes(ModItem item)
 	{
 		item.CreateRecipe(4).AddIngredient(ItemID.RichMahogany).AddTile(TileID.Sawmill).Register();
 		item.CreateRecipe(4).AddIngredient(AutoContent.ItemType<TrellisTwoMahogany>(), 4).AddTile(TileID.Sawmill).Register();
 	}
 
-	public override void SetStaticDefaults()
+	public override void AddEntry()
 	{
-		Main.wallHouse[Type] = true;
-		Main.wallLight[Type] = true;
 		DustType = DustID.RichMahogany;
-
-		var entryColor = new Color(70, 42, 44);
-		AddMapEntry(entryColor);
+		AddMapEntry(new Color(70, 42, 44));
 	}
-
-	public override void NumDust(int i, int j, bool fail, ref int num) => num = fail ? 1 : 3;
 }
 
-public class TrellisPalm : ModWall, IAutoloadWallItem
+public class TrellisPalm : Trellis
 {
-	public virtual void AddItemRecipes(ModItem item)
+	public override void AddItemRecipes(ModItem item)
 	{
 		item.CreateRecipe(4).AddIngredient(ItemID.PalmWood).AddTile(TileID.Sawmill).Register();
 		item.CreateRecipe(4).AddIngredient(AutoContent.ItemType<TrellisTwoPalm>(), 4).AddTile(TileID.Sawmill).Register();
 	}
 
-	public override void SetStaticDefaults()
+	public override void AddEntry()
 	{
-		Main.wallHouse[Type] = true;
-		Main.wallLight[Type] = true;
 		DustType = DustID.PalmWood;
-
-		var entryColor = new Color(102, 75, 34);
-		AddMapEntry(entryColor);
+		AddMapEntry(new Color(102, 75, 34));
 	}
-
-	public override void NumDust(int i, int j, bool fail, ref int num) => num = fail ? 1 : 3;
 }
 
-public class TrellisPearlwood : ModWall, IAutoloadWallItem
+public class TrellisPearlwood : Trellis
 {
-	public virtual void AddItemRecipes(ModItem item)
+	public override void AddItemRecipes(ModItem item)
 	{
 		item.CreateRecipe(4).AddIngredient(ItemID.Pearlwood).AddTile(TileID.Sawmill).Register();
 		item.CreateRecipe(4).AddIngredient(AutoContent.ItemType<TrellisTwoPearlwood>(), 4).AddTile(TileID.Sawmill).Register();
 	}
 
-	public override void SetStaticDefaults()
+	public override void AddEntry()
 	{
-		Main.wallHouse[Type] = true;
-		Main.wallLight[Type] = true;
 		DustType = DustID.Pearlwood;
-
-		var entryColor = new Color(84, 80, 75);
-		AddMapEntry(entryColor);
+		AddMapEntry(new Color(84, 80, 75));
 	}
-
-	public override void NumDust(int i, int j, bool fail, ref int num) => num = fail ? 1 : 3;
 }
 
-public class TrellisShadewood : ModWall, IAutoloadWallItem
+public class TrellisShadewood : Trellis
 {
-	public virtual void AddItemRecipes(ModItem item)
+	public override void AddItemRecipes(ModItem item)
 	{
 		item.CreateRecipe(4).AddIngredient(ItemID.Shadewood).AddTile(TileID.Sawmill).Register();
 		item.CreateRecipe(4).AddIngredient(AutoContent.ItemType<TrellisTwoShadewood>(), 4).AddTile(TileID.Sawmill).Register();
 	}
 
-	public override void SetStaticDefaults()
+	public override void AddEntry()
 	{
-		Main.wallHouse[Type] = true;
-		Main.wallLight[Type] = true;
 		DustType = DustID.Shadewood;
-
-		var entryColor = new Color(54, 62, 70);
-		AddMapEntry(entryColor);
+		AddMapEntry(new Color(54, 62, 70));
 	}
-
-	public override void NumDust(int i, int j, bool fail, ref int num) => num = fail ? 1 : 3;
 }
