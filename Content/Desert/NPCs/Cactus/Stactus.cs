538349f3e20fefa14bb46572cfa31b20af246f26
Abhay M <amanchala13@gmail.com>
2025-10-06 10:13:06 -0700

    Stactus Cannot Spawn in Sandstorms

--- Content/Desert/NPCs/Cactus/Stactus.cs
+++ Content/Desert/NPCs/Cactus/Stactus.cs
@@ -274,7 +274,7 @@ internal class Stactus : ModNPC, IDeathCount
 		}
 	}
 
-	public override float SpawnChance(NPCSpawnInfo spawnInfo) => (spawnInfo.PlayerInTown || !spawnInfo.Player.ZoneDesert || spawnInfo.SpawnTileType != TileID.Sand) ? 0 : SpawnCondition.OverworldDayDesert.Chance * 0.8f;
+	public override float SpawnChance(NPCSpawnInfo spawnInfo) => (spawnInfo.PlayerInTown || spawnInfo.Player.ZoneSandstorm || !spawnInfo.Player.ZoneDesert || spawnInfo.SpawnTileType != TileID.Sand) ? 0 : SpawnCondition.OverworldDayDesert.Chance * 0.8f;
 	public override int SpawnNPC(int tileX, int tileY)
 	{
 		var spawn = new Vector2(tileX, tileY).ToWorldCoordinates();
