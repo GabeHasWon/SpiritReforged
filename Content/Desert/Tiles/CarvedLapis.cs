8c56dc9b805f6ddb560853087907a125c3f72826
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2025-10-11 18:24:16 -0300

    Red Sandstone Slab

--- Content/Desert/Tiles/CarvedLapis.cs
+++ Content/Desert/Tiles/CarvedLapis.cs
@@ -1,5 +1,6 @@
 using SpiritReforged.Common.ItemCommon;
 using SpiritReforged.Common.TileCommon;
+using SpiritReforged.Common.TileCommon.TileMerging;
 
 namespace SpiritReforged.Content.Desert.Tiles;
 
@@ -15,9 +16,12 @@ public class CarvedLapis : ModTile, IAutoloadTileItem
 		TileID.Sets.GeneralPlacementTiles[Type] = false;
 		TileID.Sets.CanBeClearedDuringOreRunner[Type] = false;
 
+		this.Merge(ModContent.TileType<RedSandstoneBrick>(), ModContent.TileType<RedSandstoneBrickCracked>(), ModContent.TileType<RedSandstoneSlab>());
 		AddMapEntry(new Color(28, 67, 194));
 
 		DustType = DustID.Cobalt;
 		this.AutoItem().ResearchUnlockCount = 100;
 	}
+
+	public override void PostDraw(int i, int j, SpriteBatch spriteBatch) => TileMerger.DrawMerge(spriteBatch, i, j, ModContent.TileType<RedSandstoneBrick>(), ModContent.TileType<RedSandstoneBrickCracked>(), ModContent.TileType<RedSandstoneSlab>());
 }
