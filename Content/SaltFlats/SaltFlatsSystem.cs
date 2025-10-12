6e318c4d741f53813c54150c4d2eed3a22522053
GabeHasWon <gabriel@guevarameyer.com>
2025-10-11 19:29:35 -0500

    add mp sync

--- Content/SaltFlats/SaltFlatsSystem.cs
+++ Content/SaltFlats/SaltFlatsSystem.cs
@@ -1,4 +1,5 @@
 ﻿using SpiritReforged.Common.WorldGeneration;
+using System.IO;
 using Terraria.ModLoader.IO;
 
 namespace SpiritReforged.Content.SaltFlats;
@@ -10,4 +11,7 @@ internal class SaltFlatsSystem : ModSystem
 
 	public override void SaveWorldData(TagCompound tag) => tag.Add("height", SurfaceHeight);
 	public override void LoadWorldData(TagCompound tag) => SurfaceHeight = tag.GetInt("height");
+
+	public override void NetSend(BinaryWriter writer) => writer.Write((short)SurfaceHeight);
+	public override void NetReceive(BinaryReader reader) => SurfaceHeight = reader.ReadInt16();
 }
