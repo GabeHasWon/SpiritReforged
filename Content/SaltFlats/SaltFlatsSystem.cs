4dc9e8feba8a93c66b860996da64e0e830cfa418
GabeHasWon <gabriel@guevarameyer.com>
2025-10-20 17:59:16 -0500

    galaxy bg, better star sprites

--- Content/SaltFlats/SaltFlatsSystem.cs
+++ Content/SaltFlats/SaltFlatsSystem.cs
@@ -17,6 +17,7 @@ internal class SaltFlatsSystem : ModSystem
 
 	private static float _starOpacity = 0f;
 	private static Asset<Texture2D> _starTex = null;
+	private static Asset<Texture2D> _galaxyTex = null;
 
 	public override void Load()
 	{
@@ -25,6 +26,7 @@ internal class SaltFlatsSystem : ModSystem
 		if (!Main.dedServ)
 		{
 			_starTex = ModContent.Request<Texture2D>("SpiritReforged/Assets/Textures/Backgrounds/SaltFlatStar");
+			_galaxyTex = ModContent.Request<Texture2D>("SpiritReforged/Assets/Textures/Backgrounds/SaltFlatGalaxy");
 		}
 	}
 
@@ -33,33 +35,31 @@ internal class SaltFlatsSystem : ModSystem
 		orig(self, sceneArea, artificial);
 
 		if (Main.gameMenu)
-		{
 			return;
-		}
 
 		_starOpacity = MathHelper.Lerp(_starOpacity, Main.LocalPlayer.InModBiome<SaltBiome>() ? 1 : 0, 0.05f);
 
 		if (_starOpacity < 0.01f)
-		{
 			return;
-		}
 
 		if (_stars.Count == 0)
 		{
-			for (int i = 0; i < 280; ++i)
+			for (int i = 0; i < 260; ++i)
 			{
-				Vector2 pos = new(Main.rand.Next(1921), Main.rand.Next(1201));
+				Vector2 pos = new(Main.rand.Next(Main.screenWidth + 1), Main.rand.Next(Main.screenHeight + 1));
 				int frame = Main.rand.Next(6);
-				_stars.Add(new Star(frame, Main.rand.NextFloat(MathHelper.TwoPi), pos, Main.rand.NextFloat(0.3f, 0.9f), Main.rand.NextFloat(0.5f, 1.2f)));
+				_stars.Add(new Star(frame, Main.rand.NextFloat(MathHelper.TwoPi), pos, Main.rand.NextFloat(0.6f, 1f), Main.rand.NextFloat(0.5f, 1.2f)));
 			}
 		}
+
+		Main.spriteBatch.Draw(_galaxyTex.Value, new Vector2(-300), null, Color.White with { A = 0 } * _starOpacity * 0.3f, 0, Vector2.Zero, 1f, SpriteEffects.None, 0);
 
 		foreach (Star star in _stars)
 		{
 			Vector2 position = star.Position / 1200f * new Vector2(sceneArea.totalWidth, sceneArea.totalHeight)
 				+ new Vector2(0f, sceneArea.bgTopY) + sceneArea.SceneLocalScreenPositionOffset;
-			var color = Color.Lerp(Color.White, Color.DarkGray, Utils.GetLerpValue(2f, 0.4f, star.Scale, true));
-			Rectangle src = new(0, 22 * star.Frame, 20, 20);
+			var color = Color.Lerp(Color.White, Color.DarkGray, Utils.GetLerpValue(2f, 0.4f, star.Scale, true)) * _starOpacity;
+			Rectangle src = new(0, 44 * star.Frame, 40, 40);
 			float sine = 1 + MathF.Sin(Main.GameUpdateCount * 0.02f) * 0.1f;
 
 			Main.spriteBatch.Draw(_starTex.Value, position, src, color * star.Opacity * (sine - 0.2f), star.Rotation, src.Size() / 2f, star.Scale * sine, SpriteEffects.None, 0);
