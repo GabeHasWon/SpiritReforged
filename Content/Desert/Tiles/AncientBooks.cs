cf5bf87d93c3bde1966af36342ce2e6c0223399a
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2025-11-13 06:39:39 -0400

    Bookshelves

---
+++ Content/Desert/Tiles/AncientBooks.cs
@@ -0,0 +1,17 @@
+﻿namespace SpiritReforged.Content.Desert.Tiles;
+
+public class AncientBooks : ModTile
+{
+	public override void SetStaticDefaults()
+	{
+		Main.tileNoFail[Type] = true;
+		Main.tileSolid[Type] = false;
+		Main.tileFrameImportant[Type] = true;
+
+		TileObjectData.newTile.CopyFrom(TileObjectData.StyleOnTable1x1);
+		TileObjectData.newTile.StyleHorizontal = true;
+		TileObjectData.addTile(Type);
+
+		DustType = DustID.Dirt;
+	}
+}
