a5e12528149b0fcb99bf65e77d741d64abbce7d2
Eduard Krivoshapkin <buttersus@mail.ru>
2025-11-23 14:14:11 +0300

    fix: worldgen scarecrow stuck

--- Common/WorldGeneration/Micropasses/Discoveries/Passes/ScarecrowDiscovery.cs
+++ Common/WorldGeneration/Micropasses/Discoveries/Passes/ScarecrowDiscovery.cs
@@ -38,9 +38,6 @@ internal class ScarecrowDiscovery : Discovery
 
 			WorldMethods.FindGround(x, ref y);
 
-			if (y >= Main.maxTilesY - 20)
-				continue;
-
 			var tile = Main.tile[x, y];
 			if (tile.TileType == TileID.Dirt && tile.LiquidAmount == 0)
 			{
