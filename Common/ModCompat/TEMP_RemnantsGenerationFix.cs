4088cbd06b217d9173e2bbf71b5d699e962ebd1c
GabeHasWon <gabehw.contact@gmail.com>
2025-11-29 19:43:39 -0600

    temporary remnants  fix + 0.1.2.5 hotfix

---
+++ Common/ModCompat/TEMP_RemnantsGenerationFix.cs
@@ -0,0 +1,24 @@
+﻿using Terraria.DataStructures;
+using Terraria.WorldBuilding;
+
+namespace SpiritReforged.Common.ModCompat;
+
+internal class TEMP_RemnantsGenerationFix : ModSystem
+{
+	private delegate void hook_ModifyWorldGenTasks(List<GenPass> passes, ref double weight);
+
+	public override void Load() => MonoModHooks.Add(typeof(SystemLoader).GetMethod(nameof(SystemLoader.ModifyWorldGenTasks)), DetourModifyWorldGenTasks);
+
+	private static void DetourModifyWorldGenTasks(hook_ModifyWorldGenTasks orig, List<GenPass> tasks, ref double weight)
+	{
+		orig(tasks, ref weight);
+
+		if (!CrossMod.Remnants.Enabled || CrossMod.Remnants.Instance.Version != new Version(2, 0, 9))
+			return;
+
+		int index = tasks.FindIndex(x => x.Name == "[R] Enchanted Swords");
+
+		if (index != -1)
+			tasks.RemoveAt(index); // Disable doesn't work this late
+	}
+}
