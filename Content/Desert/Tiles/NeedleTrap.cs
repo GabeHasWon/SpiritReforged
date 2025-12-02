1037b3038576ec466a41e496667a27fbb04f877c
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2025-12-02 06:45:57 -0400

    Needle trap merge

--- Content/Desert/Tiles/NeedleTrap.cs
+++ Content/Desert/Tiles/NeedleTrap.cs
@@ -1,6 +1,7 @@
 using SpiritReforged.Common.Easing;
 using SpiritReforged.Common.ItemCommon;
 using SpiritReforged.Common.TileCommon;
+using SpiritReforged.Common.TileCommon.TileMerging;
 using Terraria.Audio;
 using Terraria.DataStructures;
 
@@ -22,6 +23,7 @@ public class NeedleTrap : ModTile, IAutoloadTileItem
 		Main.tileFrameImportant[Type] = true;
 
 		AddMapEntry(new Color(174, 74, 48), Language.GetText("MapObject.Trap"));
+		this.Merge(ModContent.TileType<RedSandstoneBrick>(), ModContent.TileType<RedSandstoneBrickCracked>());
 
 		DustType = DustID.DynastyShingle_Red;
 		this.AutoItem().ResearchUnlockCount = 100;
@@ -51,6 +53,8 @@ public class NeedleTrap : ModTile, IAutoloadTileItem
 		Vector2 position = tileCoords.ToWorldCoordinates(8, Framing.GetTileSafely(tileCoords).IsHalfBlock ? 16 : 8);
 		Projectile.NewProjectile(source, position, Vector2.Zero, ModContent.ProjectileType<NeedleTrapProj>(), 30, 0);
 	}
+
+	public override void PostDraw(int i, int j, SpriteBatch spriteBatch) => TileMerger.DrawMerge(spriteBatch, i, j, ModContent.TileType<RedSandstoneBrick>(), ModContent.TileType<RedSandstoneBrickCracked>());
 }
 
 public class NeedleTrapProj : ModProjectile
