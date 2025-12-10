e0557fd5d19cc4002056f288987c414c9da9a4d5
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2025-12-09 22:42:28 -0400

    Start Stone Reliquary

---
+++ Content/SaltFlats/Tiles/StoneReliquary.cs
@@ -0,0 +1,32 @@
+﻿using SpiritReforged.Common.ItemCommon;
+using SpiritReforged.Common.TileCommon.PresetTiles;
+using Terraria.DataStructures;
+
+namespace SpiritReforged.Content.SaltFlats.Tiles;
+
+public sealed class StoneReliquary : ChestTile
+{
+	public override void Load() => On_Player.IsInInteractionRangeToMultiTileHitbox += On_Player_IsInInteractionRangeToMultiTileHitbox;
+
+	private static bool On_Player_IsInInteractionRangeToMultiTileHitbox(On_Player.orig_IsInInteractionRangeToMultiTileHitbox orig, Player self, int chestPointX, int chestPointY)
+	{
+		bool value = orig(self, chestPointX, chestPointY);
+
+		if (Main.tile[chestPointX, chestPointY].TileType == AutoContent.ItemType<StoneReliquary>())
+			value = true;
+
+		return value;
+	}
+
+	public override void AddObjectData()
+	{
+		TileObjectData.newTile.CopyFrom(TileObjectData.Style3x4);
+		TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.SolidTile | AnchorType.SolidWithTop, TileObjectData.newTile.Width, 0);
+		TileObjectData.newTile.StyleHorizontal = true;
+		TileObjectData.newTile.DrawYOffset = 2;
+		TileObjectData.newTile.HookCheckIfCanPlace = new PlacementHook(Chest.FindEmptyChest, -1, 0, true);
+		TileObjectData.newTile.HookPostPlaceMyPlayer = new PlacementHook(Chest.AfterPlacement_Hook, -1, 0, false);
+		TileObjectData.newTile.AnchorInvalidTiles = [127];
+		TileObjectData.addTile(Type);
+	}
+}
