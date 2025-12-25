ada78160a01a061514f8487d8b2c707df9d080e5
GabeHasWon <gabehw.contact@gmail.com>
2025-12-24 23:36:21 -0600

    furniture set vfx & dust, hide vampires, ziggurat torch automatic re-toggle cooldown

---
+++ Common/NPCCommon/ReflectedNPC.cs
@@ -0,0 +1,17 @@
+﻿namespace SpiritReforged.Common.NPCCommon;
+
+internal class ReflectedNPC : GlobalNPC
+{
+	internal static bool ReflectingNPCs = false;
+
+	public override bool PreDraw(NPC npc, SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
+	{
+		if (!ReflectingNPCs)
+			return true;
+
+		if (npc.type is NPCID.Vampire or NPCID.VampireBat)
+			return false;
+
+		return true;
+	}
+}
