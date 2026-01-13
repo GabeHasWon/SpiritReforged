31c9ede6fd4c6bffeab85e5c3ce659ba161450a1
PhoenixBladez <amanchala13@gmail.com>
2026-01-12 16:47:51 -0800

    Increase bugs

--- Content/Ziggurat/NPCs/TinyGrub.cs
+++ Content/Ziggurat/NPCs/TinyGrub.cs
@@ -64,6 +64,6 @@ public class TinyGrub : ModNPC
 	public override float SpawnChance(NPCSpawnInfo spawnInfo)
 	{
 		int type = spawnInfo.SpawnTileType;
-		return ((type == ModContent.TileType<PaleHive>() || type == ModContent.TileType<GooeyHive>()) && Main.tile[spawnInfo.SpawnTileX, spawnInfo.SpawnTileY - 1].WallType == PaleHiveWall.UnsafeType) ? 0.2f : 0;
+		return ((type == ModContent.TileType<PaleHive>() || type == ModContent.TileType<GooeyHive>()) && Main.tile[spawnInfo.SpawnTileX, spawnInfo.SpawnTileY - 1].WallType == PaleHiveWall.UnsafeType) ? 0.62f : 0;
 	}
 }
