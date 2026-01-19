810c49970637ad11de3b294701827c05b506f1cb
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2026-01-19 08:52:45 -0400

    Remove redundant code

--- Content/Ziggurat/Tiles/ZigguratPiles.cs
+++ Content/Ziggurat/Tiles/ZigguratPiles.cs
@@ -16,20 +16,12 @@ public abstract class ZigguratPile : ModTile, IAutoloadRubble
 		Main.tileNoFail[Type] = true;
 
 		TileID.Sets.ReplaceTileBreakUp[Type] = true;
-		TileID.Sets.DoesntGetReplacedWithTileReplacement[Type] = false;
 		TileID.Sets.BreakableWhenPlacing[Type] = true;
-
-		TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
-		TileObjectData.newTile.Origin = new Point16(0, 1);
-		TileObjectData.newTile.CoordinateHeights = [16, 18];
-		TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.SolidTile | AnchorType.SolidWithTop, TileObjectData.newTile.Width, 0);
-		TileObjectData.addTile(Type);
+		TileID.Sets.DoesntGetReplacedWithTileReplacement[Type] = false;
 
 		AddMapEntry(new Color(191, 138, 67));
 		DustType = DustID.Gold;
 	}
-
-	public override void KillTile(int i, int j, ref bool fail, ref bool effectOnly, ref bool noItem) => fail = false;
 
 	public override IEnumerable<Item> GetItemDrops(int i, int j)
 	{
