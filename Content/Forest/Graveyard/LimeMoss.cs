bc4e6de3e5bbe15551fdc8ba2679d54f66aa188b
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2026-02-10 09:21:03 -0400

    Hanging vines

--- Content/Forest/Graveyard/LimeMoss.cs
+++ Content/Forest/Graveyard/LimeMoss.cs
@@ -49,5 +49,19 @@ public class LimeMoss : ModTile
 		Rectangle source = new(tile.TileFrameX, tile.TileFrameY, 16, 18);
 
 		spriteBatch.Draw(texture, new Vector2(i, j) * 16 - Main.screenPosition + new Vector2(0, 2), source, Lighting.GetColor(i, j), 0, Vector2.Zero, 1, default, 0);
+		DrawVine(i, j, spriteBatch);
+	}
+
+	private void DrawVine(int i, int j, SpriteBatch spriteBatch)
+	{
+		int length = i * j % 3;
+
+		for (int l = 0; l < length; l++)
+		{
+			Texture2D texture = TextureAssets.Tile[Type].Value;
+			Rectangle source = new((l == length - 1) ? 72 : 54, new Terraria.Utilities.FastRandom(i * j + l).Next(3) * 18, 16, 16);
+
+			spriteBatch.Draw(texture, new Vector2(i, j + l + 1) * 16 - Main.screenPosition + new Vector2(0, 2), source, Lighting.GetColor(i, j + l + 1), 0, Vector2.Zero, 1, default, 0);
+		}
 	}
 }
