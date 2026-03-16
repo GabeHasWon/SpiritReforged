8d5072044d82b84f4ecfd6d96e7d8a537b1e8f0a
SkippZzMods <pumpkinboo2k6@gmail.com>
2026-03-15 21:13:07 -0400

    finished ver

--- Content/Desert/ScarabBoss/Items/RoyalKhopesh.cs
+++ Content/Desert/ScarabBoss/Items/RoyalKhopesh.cs
@@ -1,19 +1,25 @@
+using SpiritReforged.Common.Misc;
 using SpiritReforged.Common.ModCompat;
+using SpiritReforged.Common.Particle;
 using SpiritReforged.Common.ProjectileCommon;
 using SpiritReforged.Content.Desert.ScarabBoss.Items.Projectiles;
+using SpiritReforged.Content.Particles;
+using SpiritReforged.Content.Underground.Items.BigBombs;
+using System.IO;
+using Terraria.Audio;
 using Terraria.DataStructures;
 
 namespace SpiritReforged.Content.Desert.ScarabBoss.Items;
 
 public class RoyalKhopesh : ModItem
 {
-	public override bool IsLoadingEnabled(Mod mod) => false;
+	internal int swingDirection = 1;
 
 	public override void SetDefaults()
 	{
 		Item.damage = 28;
 		Item.Size = new Vector2(48, 52);
-		Item.useTime = Item.useAnimation = 40;
+		Item.useTime = Item.useAnimation = 48;
 		Item.knockBack = 1f;
 		Item.shootSpeed = 1;
 		Item.noMelee = true;
@@ -33,64 +39,122 @@ public class RoyalKhopesh : ModItem
 
 	public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
 	{
-		PreNewProjectile.New(source, player.Center, Vector2.Zero, Item.shoot, Item.damage, Item.knockBack, player.whoAmI, preSpawnAction: (projectile) =>
+		player.TryGetModPlayer<RoyalKhopeshPlayer>(out var modPlayer);
+
+		if (player.altFunctionUse == 2)
 		{
-			KhopeshPlayer kPlayer = player.GetModPlayer<KhopeshPlayer>();
-			var khopesh = projectile.ModProjectile as RoyalKhopeshHeld;
+			modPlayer.ThrowCooldown = RoyalKhopeshPlayer.COOLDOWN_TIME;
 
-			if (kPlayer.Combo != 2) //Don't use standard item use sound on use, allow the final swing to do it at a specified time
-				khopesh.DoSwingNoise();
+			type = ModContent.ProjectileType<RoyalKhopeshThrown>();
+			velocity *= 20f;
 
-			int useSpeed = (int)(Item.useTime / player.GetTotalAttackSpeed(DamageClass.Melee));
-			khopesh.BaseDirection = velocity;
-			khopesh.SwingDirection = kPlayer.Combo % 3 == 1 ? -1 : 1;
-			khopesh.SwingTime = kPlayer.Combo == 2 ? (int)(useSpeed * 1.7f) : useSpeed;
-			khopesh.SwingTime *= 1 + RoyalKhopeshHeld.EXTRA_UPDATES;
-			khopesh.Combo = kPlayer.Combo;
-			khopesh.SwingRadians = MathHelper.Pi * 1.75f;
+			SoundEngine.PlaySound(SoundID.DD2_MonkStaffSwing, position);
+		}		
+		else
+			swingDirection *= -1;
 
-			float scale = Item.scale;
-			player.ApplyMeleeScale(ref scale);
-			khopesh.SizeModifier = scale;
+		Projectile.NewProjectile(source, position, velocity, type, damage, knockback, player.whoAmI, swingDirection);
 
-			kPlayer.IncrementCombo();
-		});
-
+		if (modPlayer.FastStrikeAmount <= 0)
+			modPlayer.Combo++;
+		
 		return false;
 	}
 
-	public override bool CanUseItem(Player player) => player.ownedProjectileCounts[Item.shoot] == 0;
+	public override bool AltFunctionUse(Player player) => player.ownedProjectileCounts[ModContent.ProjectileType<RoyalKhopeshThrown>()] <= 0 && player.GetModPlayer<RoyalKhopeshPlayer>().ThrowCooldown <= 0;
 
+	public override bool CanUseItem(Player player) => player.ownedProjectileCounts[Item.shoot] <= 0;
 	public override bool MeleePrefix() => true;
+
+	public override void NetReceive(BinaryReader reader) => swingDirection = reader.ReadInt32();
+
+	public override void NetSend(BinaryWriter writer) => writer.Write(swingDirection);
 }
 
-public class KhopeshPlayer : ModPlayer
+public class RoyalKhopeshPlayer : ModPlayer
 {
-	public int Combo { get; set; }
+	public const int COOLDOWN_TIME = 300;
+	public const int COOLDOWN_FLASH_TIMER = 20;
 
-	private int _comboTimer;
+	private static readonly SoundStyle CooldownSound = SoundID.Item29 with { Volume = 0.2f, PitchVariance = 0.1f };
 
-	private const int COMBOTIMER_MAX = 60;
+	public int Combo;
+	public int FastStrikeAmount;
+	public int EmpoweredStrikeTimer;
+	public int ThrowCooldown;
 
-	public override bool IsLoadingEnabled(Mod mod) => false;
+	internal int CooldownFlashTimer;
+
+	// not entirely sure if I need to unload
+	public override void Load() => On_Main.DrawInfernoRings += DrawCooldownFlash;
+	private void DrawCooldownFlash(On_Main.orig_DrawInfernoRings orig, Main self)
+	{
+		orig(self);
+
+		foreach (Player p in Main.player)
+		{
+			p.TryGetModPlayer<RoyalKhopeshPlayer>(out var modPlayer);
+
+			if (modPlayer?.CooldownFlashTimer > 0)
+			{
+				Texture2D star = AssetLoader.LoadedTextures["Star"].Value;
+				Texture2D bloom = AssetLoader.LoadedTextures["Bloom"].Value;
+
+				float flashTime = modPlayer.CooldownFlashTimer / (float)COOLDOWN_FLASH_TIMER;
+				
+				Main.spriteBatch.Draw(bloom, (p.HandPosition ?? p.Center) - Main.screenPosition, null, Color.MediumVioletRed.Additive() * 0.5f * flashTime, 0f, bloom.Size() / 2f, 0.3f, 0f, 0f);
+
+				Main.spriteBatch.Draw(star, (p.HandPosition ?? p.Center) - Main.screenPosition, null, Color.MediumVioletRed.Additive(), 0f, star.Size() / 2f, 0.3f * flashTime, 0f, 0f);
+
+				Main.spriteBatch.Draw(star, (p.HandPosition ?? p.Center) - Main.screenPosition, null, Color.White.Additive(), 0f, star.Size() / 2f, 0.15f * flashTime, 0f, 0f);
+			}
+		}
+	}
 
 	public override void ResetEffects()
 	{
-		_comboTimer = Math.Max(--_comboTimer, 0);
-		if (Player.HeldItem.type != ModContent.ItemType<RoyalKhopesh>())
+		if (EmpoweredStrikeTimer > 0)
+			EmpoweredStrikeTimer--;
+
+		if (ThrowCooldown > 0)
 		{
-			_comboTimer = 0;
-			Combo = 0;
+			ThrowCooldown--;
+			if (ThrowCooldown == 0)
+			{
+				SoundEngine.PlaySound(CooldownSound, Player.Center);
+				CooldownFlashTimer = COOLDOWN_FLASH_TIMER;
+			}
 		}
 
-		else if (_comboTimer == 0)
-			Combo = 0;
-	}
+		if (CooldownFlashTimer > 0)
+			CooldownFlashTimer--;
 
-	public void IncrementCombo()
+		if (Combo > 2)
+		{
+			Combo = 0;
+			FastStrikeAmount = 2;
+		}			
+	}
+}
+
+public class RoyalKhopeshGlobalNPC : GlobalNPC
+{
+	public Vector2? targetPosition;
+	public int slowTimer;
+	public override bool InstancePerEntity => true;
+
+	public override void AI(NPC npc)
 	{
-		Combo++;
-		_comboTimer = COMBOTIMER_MAX;
-		Combo %= 3;
+		if (targetPosition != null && slowTimer > 0)
+		{
+			slowTimer--;
+
+			float dist = npc.Distance(targetPosition.Value);
+
+			npc.velocity *= MathHelper.Lerp(0.85f, 0.99f, dist / 400f);
+
+			if (dist is < 10f or > 400f)
+				targetPosition = null;
+		}
 	}
 }
