d96ed21e627fc027869c1d4eea2936b9eccee1a5
GabeHasWon <gabehw.contact@gmail.com>
2026-03-20 22:39:06 -0500

    functionally finished, needs mp testing to confirm good to go

--- Content/Desert/ScarabBoss/Items/SpaceHeater.cs
+++ Content/Desert/ScarabBoss/Items/SpaceHeater.cs
@@ -1,13 +1,61 @@
-﻿using SpiritReforged.Content.Desert.ScarabBoss.Boss;
+﻿using SpiritReforged.Common.Multiplayer;
+using SpiritReforged.Common.TileCommon;
+using SpiritReforged.Content.Desert.ScarabBoss.Boss;
+using System.IO;
+using Terraria.Audio;
 using Terraria.DataStructures;
 using Terraria.GameContent.ObjectInteractions;
-using Terraria.GameContent.UI.Elements;
-using Terraria.UI;
+using Terraria.GameInput;
+using Terraria.ModLoader.IO;
 
 namespace SpiritReforged.Content.Desert.ScarabBoss.Items;
 
 internal class SpaceHeater : ModItem
 {
+	public class SpaceHeaterPacketData : PacketData
+	{
+		private readonly int _id;
+		private readonly float _value;
+		private readonly int _from;
+
+		public SpaceHeaterPacketData()
+		{
+		}
+
+		public SpaceHeaterPacketData(int id, float value, int from)
+		{
+			_id = id;
+			_value = value;
+			_from = from;
+		}
+
+		public override void OnSend(ModPacket modPacket)
+		{
+			modPacket.Write((short)_id);
+			modPacket.Write((Half)_value);
+
+			if (Main.netMode != NetmodeID.Server)
+				modPacket.Write((byte)_from);
+		}
+
+		public override void OnReceive(BinaryReader reader, int whoAmI)
+		{
+			short id = reader.ReadInt16();
+			float value = (float)reader.ReadHalf();
+
+			if (TileEntity.ByID[id] is not SpaceHeaterEntity entity)
+				return;
+
+			entity.Strength = value;
+
+			if (Main.netMode == NetmodeID.Server)
+			{
+				byte from = reader.ReadByte();
+				Send(-1, from);
+			}
+		}
+	}
+
 	public sealed class SpaceHeaterEntity : ModTileEntity
 	{
 		/// <summary>
@@ -22,10 +70,15 @@ internal class SpaceHeater : ModItem
 		}
 
 		public override int Hook_AfterPlacement(int i, int j, int type, int style, int direction, int alternate) => Generic_Hook_AfterPlacement(i, j, type, style, direction, alternate);
+
+		public override void SaveData(TagCompound tag) => tag.Add("str", Strength);
+		public override void LoadData(TagCompound tag) => Strength = tag.GetFloat("str");
 	}
 
 	public sealed class SpaceHeaterTile : ModTile
 	{
+		private bool LastFrameModified = false;
+
 		public override void SetStaticDefaults()
 		{
 			Main.tileFrameImportant[Type] = true;
@@ -35,8 +88,8 @@ internal class SpaceHeater : ModItem
 			TileID.Sets.DisableSmartCursor[Type] = true;
 
 			TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
-			TileObjectData.newTile.Height = 3;
-			TileObjectData.newTile.CoordinateHeights = [16, 16, 18];
+			TileObjectData.newTile.Height = 4;
+			TileObjectData.newTile.CoordinateHeights = [16, 16, 16, 18];
 			TileObjectData.newTile.Origin = new Point16(0, 2);
 			TileObjectData.newTile.LavaDeath = false;
 			var tileEntity = ModContent.GetInstance<SpaceHeaterEntity>();
@@ -53,11 +106,65 @@ internal class SpaceHeater : ModItem
 
 		public override void MouseOver(int i, int j)
 		{
+			Point16 topLeft = TileObjectData.TopLeft(i, j);
 			Player player = Main.LocalPlayer;
-			player.noThrow = 2;
-			player.cursorItemIconEnabled = true;
-			player.cursorItemIconID = ModContent.ItemType<SpaceHeater>();
+
+			if (Main.mouseRight && TileEntity.ByPosition[topLeft] is SpaceHeaterEntity heater)
+			{
+				float factor = Utils.GetLerpValue(topLeft.Y * 16, (topLeft.Y + 4) * 16, Main.MouseWorld.Y, true);
+				heater.Strength = 1 - factor;
+
+				if (Main.netMode == NetmodeID.MultiplayerClient)
+					SyncHeater(heater.ID, heater.Strength);
+
+				Main.blockMouse = true;
+				LastFrameModified = true;
+			}
+			else
+			{
+				if (LastFrameModified)
+					SoundEngine.PlaySound(SoundID.MenuTick);
+
+				LastFrameModified = false;
+
+				player.noThrow = 2;
+				player.cursorItemIconEnabled = true;
+				player.cursorItemIconID = ModContent.ItemType<SpaceHeater>();
+			}
 		}
+
+		public override bool RightClick(int i, int j)
+		{
+			Point16 topLeft = TileObjectData.TopLeft(i, j);
+			TileObjectData data = TileObjectData.GetTileData(Main.tile[i, j]);
+
+			if (TileEntity.ByPosition[topLeft] is not SpaceHeaterEntity heater)
+				return false;
+
+			bool smartContainsX = topLeft.X >= Main.SmartCursorX && Main.SmartCursorX < topLeft.X + data.Width;
+			bool smartContainsY = topLeft.Y >= Main.SmartCursorY && Main.SmartCursorY < topLeft.Y + data.Height;
+
+			if (smartContainsX && smartContainsY) // "Hijack" smart cursor functionality to move from 0% -> 25 -> 50 -> 75 -> 100 -> 0%
+			{
+				heater.Strength = heater.Strength switch
+				{
+					< 0.25f => 0.25f,
+					< 0.5f => 0.5f,
+					< 0.75f => 0.75f,
+					< 1 => 1,
+					_ => 0,
+				};
+
+				if (Main.netMode == NetmodeID.MultiplayerClient)
+					SyncHeater(heater.ID, heater.Strength);
+
+				SoundEngine.PlaySound(SoundID.MenuTick);
+			}
+
+			return true;
+		}
+
+		private static void SyncHeater(int id, float strength) => new SpaceHeaterPacketData(id, strength, Main.myPlayer).Send();
 
 		public override void NearbyEffects(int i, int j, bool closer)
 		{
@@ -65,6 +172,55 @@ internal class SpaceHeater : ModItem
 
 			if (TileEntity.ByPosition[topLeft] is SpaceHeaterEntity heater)
 				HeaterSystem.HeatEffect = heater.Strength;
+		}
+
+		public override void PostDraw(int i, int j, SpriteBatch spriteBatch)
+		{
+			TileObjectData data = TileObjectData.GetTileData(Main.tile[i, j]);
+			bool isBottomRight = TileObjectData.IsTopLeft(i - data.Width + 1, j - data.Height + 1);
+			Point16 topLeft = TileObjectData.TopLeft(i, j);
+			bool hideSlider = !TileExtensions.GetVisualInfo(i, j, out Color color, out Texture2D texture);
+
+			if (!isBottomRight || TileEntity.ByPosition[topLeft] is not SpaceHeaterEntity heater)
+				return;
+
+			Vector2 position = TileExtensions.DrawPosition(i - data.Width + 1, j - data.Height + 1, new Vector2(-10, heater.Strength * 50 - 54));
+
+			if (!hideSlider)
+			{
+				Rectangle button = new(0, 74, 12, 8);
+				spriteBatch.Draw(texture, position, button, color);
+			}
+
+			if (heater.Strength > 0)
+			{
+				Rectangle glow = new(36, 0, 16, 16);
+
+				for (int x = 0; x < data.Width; ++x)
+				{
+					for (int y = 0; y < data.Height; ++y)
+					{
+						if (TileExtensions.GetVisualInfo(topLeft.X + x, topLeft.Y + y, out Color tileColor, out Texture2D individualTileTexture))
+						{
+							glow.X = 36 + 18 * x;
+							glow.Y = 0 + 18 * y;
+							spriteBatch.Draw(individualTileTexture, TileExtensions.DrawPosition(i + x, j + y, new Vector2(16, 48)), glow, tileColor * heater.Strength);
+						}
+					}
+				}
+			}
+
+			if (hideSlider)
+				return;
+
+			PlayerInput.SetZoom_MouseInWorld();
+			Point mouse = Main.MouseWorld.ToPoint();
+			PlayerInput.SetZoom_UI();
+
+			Rectangle tileBounds = new(topLeft.X * 16, topLeft.Y * 16, 16 * 2, 16 * 4);
+
+			if (tileBounds.Contains(mouse))
+				spriteBatch.Draw(texture, position - new Vector2(2, 2), new Rectangle(14, 72, 16, 12), color);
 		}
 	}
 
@@ -78,39 +234,9 @@ internal class SpaceHeater : ModItem
 		{
 			if (HeatEffect is { } value)
 			{
-				ScarabHeatHazeShaderData.HeatHazeTargetIntensity = value;
-				ScarabHeatHazeShaderData.HeatHazeTargetOpacity = value;
-			}
-		}
-	}
-
-	public class HeaterUI : UIState
-	{
-		public static Point16 WorldAnchor = Point16.Zero;
-
-		private static Asset<Texture2D> UI = null;
-		private static float Progress = 0;
-
-		public override void OnInitialize()
-		{
-			UI ??= ModContent.Request<Texture2D>("SpiritReforged/Contents/Desert/ScarabBoss/Items/SpaceHeater/SpaceHeaterUI");
-
-			UIImageFramed back = new(UI, new Rectangle(0, 0, 8, 48));
-			Append(back);
-
-			UIImageFramed slider = new(UI, new Rectangle(10, 0, 12, 12));
-			slider.OnUpdate += HoverSliderUpdate;
-
-		}
-
-		private void HoverSliderUpdate(UIElement affectedElement)
-		{
-			UIImageFramed slider = affectedElement as UIImageFramed;
-			slider.SetFrame(slider.IsMouseHovering ? new Rectangle(10, 14, 12, 12) : new Rectangle(10, 0, 12, 12));
-
-			if (IsMouseHovering && Main.mouseLeft)
-			{
-				Progress = 0;
+				// Opacity goes from 0 -> 1 at value = 0 .. 0.5, then Intensity goes from 0 -> 1 at value = 0.5 .. 1
+				ScarabHeatHazeShaderData.HeatHazeTargetIntensity = MathF.Max(0, (value - 0.5f) * 2f);
+				ScarabHeatHazeShaderData.HeatHazeTargetOpacity = MathF.Min(value * 2, 1);
 			}
 		}
 	}
