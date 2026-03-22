f7959b076452ee40b9eafe73da72b663dd83976a
PhoenixBladez <amanchala13@gmail.com>
2026-03-22 15:22:33 -0700

    Club damage tweaks

--- Content/Ocean/Items/BassClub/BassSlapper.cs
+++ Content/Ocean/Items/BassClub/BassSlapper.cs
@@ -5,18 +5,19 @@ namespace SpiritReforged.Content.Ocean.Items.BassClub;
 
 public class BassSlapper : ClubItem
 {
-	internal override float DamageScaling => 1.25f;
+	internal override float DamageScaling => 1.875f;
+	internal override float KnockbackScaling => 1.4f;
 
 	public override void SetStaticDefaults() => MoRHelper.AddElement(Item, MoRHelper.Water, true);
 	public override void SafeSetDefaults()
 	{
-		Item.damage = 26;
+		Item.damage = 30;
 		Item.knockBack = 14;
 		ChargeTime = 50;
 		SwingTime = 26;
 		Item.width = 60;
 		Item.height = 60;
-		Item.crit = 4;
+		Item.crit = 8;
 		Item.value = Item.sellPrice(0, 0, 60, 0);
 		Item.rare = ItemRarityID.Green;
 		Item.shoot = ModContent.ProjectileType<BassSlapperProj>();
