5067793ef7640d088413d6094b88f0e7b6e135be
GabeHasWon <gabehw.contact@gmail.com>
2026-03-23 00:01:10 -0500

    ziggurat counts as desert, make corrupt sands count as desert instead of corruption

--- Content/Ziggurat/Biome/ZigguratBiome.cs
+++ Content/Ziggurat/Biome/ZigguratBiome.cs
@@ -1,10 +1,31 @@
 ﻿using SpiritReforged.Content.Ziggurat.Tiles;
 using SpiritReforged.Content.Ziggurat.Walls;
+using System.Runtime.CompilerServices;
 
 namespace SpiritReforged.Content.Ziggurat.Biome;
 
 public class ZigguratBiome : ModBiome
 {
+	public class ZigguratCounts : ILoadable
+	{
+		public void Load(Mod mod) => MonoModHooks.Add(typeof(TileLoader).GetMethod(nameof(TileLoader.RecountTiles)), RecountTiles);
+
+		public static void RecountTiles(Action<SceneMetrics> orig, SceneMetrics metrics)
+		{
+			orig(metrics);
+
+			int[] tileCounts = GetTileCounts(metrics);
+			metrics.SandTileCount += tileCounts[ModContent.TileType<RedSandstoneBrick>()] * 2;
+			metrics.SandTileCount += tileCounts[ModContent.TileType<RedSandstoneBrickCracked>()] * 2;
+			metrics.SandTileCount += tileCounts[ModContent.TileType<RedSandstoneSlab>()] * 2;
+		}
+
+		[UnsafeAccessor(UnsafeAccessorKind.Field, Name = "_tileCounts")]
+		public static extern ref int[] GetTileCounts(SceneMetrics metrics);
+
+		public void Unload() { }
+	}
+
 	/// <summary> A collection of walls associated with the natural biome. </summary>
 	public static readonly HashSet<int> WallTypes = [RedSandstoneBrickWall.UnsafeType, ModContent.WallType<RedSandstoneBrickForegroundWall>(), CarvedLapisWall.UnsafeType, PaleHiveWall.UnsafeType, ModContent.WallType<SandyZigguratWall>(), BronzePlatingWall.UnsafeType];
 	/// <summary> A collection of tiles associated with the natural biome. Used only for <see cref="Common.WorldGeneration.QuickConversion."/> </summary>
@@ -20,4 +41,6 @@ public class ZigguratBiome : ModBiome
 		int wallType = Framing.GetTileSafely(player.Center).WallType;
 		return WallTypes.Contains(wallType);
 	}
+
+	public override void OnInBiome(Player player) => player.ZoneDesert = true;
 }
