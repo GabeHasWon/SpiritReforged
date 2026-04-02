629cfa76d3aa00d832749ce6de4b823e55aaf8bb
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2026-04-02 00:59:32 -0300

    Null fix

--- Content/Forest/Glyphs/BlankGlyph.cs
+++ Content/Forest/Glyphs/BlankGlyph.cs
@@ -1,5 +1,7 @@
 using Humanizer;
+using SpiritReforged.Common.Misc;
 using SpiritReforged.Common.ModCompat.Classic;
+using SpiritReforged.Common.Visuals;
 using System.IO;
 using Terraria.Audio;
 using Terraria.ModLoader.IO;
@@ -60,19 +62,30 @@ public class GlyphGlobalItem : GlobalItem
 
 	public override bool InstancePerEntity => true;
 
+	/// <summary> Prevents item consumption for the local client only. </summary>
+	private static bool StopItemConsumption;
 	public GlyphType glyph;
 
-	public override bool CanRightClick(Item item) => Main.LocalPlayer.HeldItem.ModItem is GlyphItem glyphItem && glyphItem.CanBeApplied(item);
+	public override bool CanRightClick(Item item) => Main.mouseItem.ModItem is GlyphItem glyphItem && glyphItem.CanBeApplied(item);
 
 	public override void RightClick(Item item, Player player)
 	{
-		if (player.HeldItem.ModItem is GlyphItem glyphItem && glyphItem.CanBeApplied(item))
+		if (Main.mouseItem.ModItem is GlyphItem glyphItem && glyphItem.CanBeApplied(item))
 		{
-			glyphItem.OnApply(item, player);
+			glyphItem.OnApply(item);
 
 			if (--Main.mouseItem.stack <= 0)
 				Main.mouseItem.TurnToAir(); //Consume a glyph on hand
+
+			StopItemConsumption = true;
 		}
+	}
+
+	public override bool ConsumeItem(Item item, Player player)
+	{
+		bool value = StopItemConsumption;
+		StopItemConsumption = false;
+		return !value;
 	}
 
 	public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
@@ -97,6 +110,9 @@ public class GlyphGlobalItem : GlobalItem
 			Texture2D texture = glyphItem.IconTexture.Value;
 			float iconScale = Main.inventoryScale;
 			Vector2 iconPosition = position + (new Vector2(slotDimensions / 2, slotDimensions / 2) - texture.Size() / 2 - new Vector2(4)) * iconScale;
+
+			DrawHelpers.DrawOutline(spriteBatch, texture, iconPosition, Color.White, (offset) =>
+				spriteBatch.Draw(texture, iconPosition + offset, null, Color.White.Additive() * ((1f + (float)Math.Sin(Main.timeForVisualEffects / 30f)) * 0.1f), 0, texture.Size() / 2, iconScale, 0, 0));
 
 			spriteBatch.Draw(texture, iconPosition, null, Color.White, 0, texture.Size() / 2, iconScale, 0, 0);
 		}
@@ -147,17 +163,20 @@ public abstract class GlyphItem : ModItem
 	private const string glyphLocalization = "Mods.SpiritReforged.Items.BlankGlyph.";
 	private static readonly Dictionary<int, Asset<Texture2D>> IconByItemType = [];
 
-	public static readonly SoundStyle EnchantSound = new("SpiritReforged/SFX/Item/GlyphAttach");
+	public static readonly SoundStyle EnchantSound = new("SpiritReforged/Assets/SFX/Item/GlyphAttach");
 	public GlyphSettings settings;
 
 	public override void SetStaticDefaults() => Item.ResearchUnlockCount = 5;
 
 	public virtual bool CanBeApplied(Item item) => item.damage >= 0 && item.TryGetGlobalItem(out GlyphGlobalItem glyphItem) && glyphItem.glyph.ItemType != Type;
 
-	public virtual void OnApply(Item item, Player player)
+	public virtual void OnApply(Item item)
 	{
 		item.GetGlobalItem<GlyphGlobalItem>().glyph = new(Type);
-		SoundEngine.PlaySound(EnchantSound, player.Center);
+		SoundEngine.PlaySound(EnchantSound);
+
+		item.ClearNameOverride();
+		item.SetNameOverride($"{Effect} " + item.Name);
 	}
 
 	public override void ModifyTooltips(List<TooltipLine> tooltips)
---
+++ Content/Forest/Glyphs/NullGlyph.cs
@@ -0,0 +1,37 @@
+using Terraria.Audio;
+
+namespace SpiritReforged.Content.Forest.Glyphs;
+
+public sealed class NullGlyph : GlyphItem
+{
+	public override bool CanBeApplied(Item item) => item.TryGetGlobalItem(out GlyphGlobalItem glyphItem) && !glyphItem.glyph.Empty;
+
+	public override void OnApply(Item item)
+	{
+		item.GetGlobalItem<GlyphGlobalItem>().glyph = default;
+		SoundEngine.PlaySound(EnchantSound);
+
+		item.ClearNameOverride();
+	}
+
+	public override void SetDefaults()
+	{
+		Item.width = 28;
+		Item.height = 28;
+		Item.value = Item.buyPrice(0, 5, 0, 0);
+		Item.rare = ItemRarityID.Blue;
+		Item.maxStack = Item.CommonMaxStack;
+		settings = new(new() { PackedValue = 0x9f9593 });
+	}
+
+	public override void ModifyTooltips(List<TooltipLine> tooltips)
+	{
+		if (tooltips.FindIndex(static x => x.Name == "Tooltip0") is int index && index < 0)
+			return;
+
+		tooltips.Insert(index, new(Mod, "GlyphHint", (Item.shopCustomPrice.HasValue ? Target : this.GetLocalization("RightClick")).Value)
+		{
+			OverrideColor = new Color(120, 190, 120)
+		});
+	}
+}
