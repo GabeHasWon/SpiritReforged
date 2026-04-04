6f50a12191141cde1e3e5bf889029cd5f6c7fbb5
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2026-04-03 21:43:38 -0300

    Start Enchanted Anvil

---
+++ Common/UI/EnchantedAnvilState.cs
@@ -0,0 +1,152 @@
+﻿using SpiritReforged.Common.Easing;
+using SpiritReforged.Common.Misc;
+using SpiritReforged.Common.UI.Misc;
+using SpiritReforged.Common.UI.System;
+using SpiritReforged.Common.Visuals;
+using SpiritReforged.Content.Forest.Glyphs;
+using System.Linq;
+using Terraria.UI;
+
+namespace SpiritReforged.Common.UI;
+
+public class EnchantedAnvilState : AutoUIState
+{
+	public class GlyphButton : UIElement
+	{
+		private readonly int _itemType;
+
+		public GlyphButton(int itemType)
+		{
+			_itemType = itemType;
+
+			Width.Set(28, 0);
+			Height.Set(28, 0);
+		}
+
+		public override void Update(GameTime gameTime)
+		{
+			if (IsMouseHovering)
+			{
+				if (Main.mouseLeft && Main.mouseLeftRelease)
+				{
+
+				}
+			}
+		}
+
+		public override void Draw(SpriteBatch spriteBatch)
+		{
+			Texture2D texture = TextureAssets.Item[_itemType].Value;
+			Vector2 center = GetDimensions().Center();
+
+			if (IsMouseHovering)
+			{
+				DrawHelpers.DrawOutline(spriteBatch, texture, center, Color.White, (offset) =>
+				{
+					Texture2D outlineTexture = TextureColorCache.ColorSolid(texture, Color.White);
+					Color outlineColor = ItemLoader.GetItem(_itemType) is GlyphItem glyphItem ? glyphItem.settings.Color : Color.White;
+
+					spriteBatch.Draw(outlineTexture, center + offset, null, outlineColor, 0, texture.Size() / 2, 1, 0, 0);
+				});
+			}
+
+			spriteBatch.Draw(texture, center, null, Color.White, 0, texture.Size() / 2, 1, 0, 0);
+		}
+	}
+
+	private BasicItemSlot _slot;
+	private float _animationProgress;
+	private readonly List<GlyphButton> _glyphButtons = [];
+
+	public override void OnInitialize()
+	{
+		Width = Height = StyleDimension.Fill;
+
+		_slot = new(new Item());
+		_slot.Left = new StyleDimension(-(_slot.Width.Pixels / 2), 0.5f);
+		_slot.Top = new StyleDimension(-(_slot.Height.Pixels / 2), 0.5f);
+		OverrideSamplerState = SamplerState.PointClamp;
+		Append(_slot);
+	}
+
+	public override void Update(GameTime gameTime)
+	{
+		if (Main.LocalPlayer.controlInv || !Main.playerInventory)
+			UISystem.SetInactive<EnchantedAnvilState>();
+
+		if (_slot.Item.IsAir)
+		{
+			if (_glyphButtons.Count != 0) //Remove all buttons
+				RemoveButtons();
+
+			_animationProgress = 0;
+		}
+		else
+		{
+			if (_glyphButtons.Count == 0) //Add all buttons
+				AddButtons();
+
+			_animationProgress = MathHelper.Min(_animationProgress + 0.025f, 1);
+		}
+
+		int count = _glyphButtons.Count;
+		for (int i = 0; i < count; i++)
+		{
+			GlyphButton button = _glyphButtons[i];
+			Vector2 origin = button.GetDimensions().ToRectangle().Size() / 2;
+
+			float rotation = MathHelper.PiOver2 * ((float)(i / (count - 1f)) - 0.5f);
+			Vector2 targetPosition = _slot.GetDimensions().Center() + new Vector2(0, -100).RotatedBy(rotation);
+
+			var lerpPosition = Vector2.Lerp(button.GetDimensions().Center(), targetPosition, EaseFunction.EaseCircularInOut.Ease(_animationProgress)) - origin; //Lerp to position
+			button.Left.Set(lerpPosition.X, 0);
+			button.Top.Set(lerpPosition.Y, 0);
+		}
+
+		base.Update(gameTime);
+	}
+
+	private void AddButtons()
+	{
+		CreateGlyphs(3, out int[] itemTypes);
+
+		for (int i = 0; i < itemTypes.Length; i++)
+		{
+			GlyphButton button = new(itemTypes[i]);
+			button.Left.Set(0, 0.5f);
+			button.Top.Set(0, 0.5f);
+
+			_glyphButtons.Add(button);
+			Append(button);
+		}
+	}
+
+	private void RemoveButtons()
+	{
+		for (int i = _glyphButtons.Count - 1; i >= 0; i--)
+		{
+			GlyphButton button = _glyphButtons[i];
+			_glyphButtons.Remove(button);
+			RemoveChild(button);
+		}
+	}
+
+	private static void CreateGlyphs(int count, out int[] itemTypes)
+	{
+		List<GlyphItem> glyphItems = ModContent.GetContent<GlyphItem>().ToList();
+		List<int> result = [];
+
+		for (int c = 0; c < count; c++)
+		{
+			var choice = glyphItems[Main.rand.Next(glyphItems.Count)];
+
+			result.Add(choice.Type);
+			glyphItems.Remove(choice);
+
+			if (glyphItems.Count == 0)
+				break;
+		}
+
+		itemTypes = result.ToArray();
+	}
+}
---
+++ Content/Underground/Tiles/EnchantedAnvil.cs
@@ -0,0 +1,65 @@
+using SpiritReforged.Common.Misc;
+using SpiritReforged.Common.TileCommon;
+using SpiritReforged.Common.UI;
+using SpiritReforged.Common.UI.System;
+using SpiritReforged.Common.Visuals.Glowmasks;
+using Terraria.DataStructures;
+
+namespace SpiritReforged.Content.Underground.Tiles;
+
+[AutoloadGlowmask("255,255,255", false)]
+public class EnchantedAnvil : ModTile
+{
+	public override void SetStaticDefaults()
+	{
+		Main.tileFrameImportant[Type] = true;
+		Main.tileOreFinderPriority[Type] = 600;
+		Main.tileSpelunker[Type] = true;
+		Main.tileNoFail[Type] = true;
+
+		TileObjectData.newTile.CopyFrom(TileObjectData.Style2x1);
+		TileObjectData.newTile.Origin = new(0, 1);
+		TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.SolidTile | AnchorType.SolidWithTop, TileObjectData.newTile.Width, 0);
+		TileObjectData.newTile.DrawYOffset = 2;
+		TileObjectData.addTile(Type);
+
+		DustType = -1;
+
+		AddMapEntry(new Color(50, 25, 55), CreateMapEntryName());
+		RegisterItemDrop(ItemID.IronAnvil, 0);
+		RegisterItemDrop(ItemID.LeadAnvil, 1);
+	}
+
+	public override void MouseOver(int i, int j) => base.MouseOver(i, j);
+
+	public override bool RightClick(int i, int j)
+	{
+		UISystem.SetActive<EnchantedAnvilState>();
+		Main.playerInventory = true;
+
+		return true;
+	}
+
+	public override void NearbyEffects(int i, int j, bool closer)
+	{
+		if (!closer && TileObjectData.IsTopLeft(i, j))
+		{
+			if (Main.LocalPlayer.DistanceSQ(new Vector2(i + 1, j) * 16) > 100 * 100)
+				UISystem.SetInactive<EnchantedAnvilState>();
+		}
+	}
+
+	public override void EmitParticles(int i, int j, Tile tile, short tileFrameX, short tileFrameY, Color tileLight, bool visible)
+	{
+
+	}
+
+	public override void PostDraw(int i, int j, SpriteBatch spriteBatch)
+	{
+		Tile tile = Main.tile[i, j];
+		Texture2D texture = GlowmaskTile.TileIdToGlowmask[Type].Glowmask.Value;
+
+		spriteBatch.Draw(texture, new Vector2(i, j) * 16 - Main.screenPosition + TileExtensions.TileOffset + new Vector2(0, 2), 
+			new Rectangle(tile.TileFrameX, tile.TileFrameY, 16, 16), Color.Cyan, 0, Vector2.Zero, 1, 0, 0);
+	}
+}
