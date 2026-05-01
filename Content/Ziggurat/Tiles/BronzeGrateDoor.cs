1403e2a2cf5a121961f953c19b32c106c5a7b75b
PhoenixBladez <amanchala13@gmail.com>
2026-04-30 23:19:25 -0700

    Bronze Grate Door now allows water thru

--- Content/Ziggurat/Tiles/BronzeGrateDoor.cs
+++ Content/Ziggurat/Tiles/BronzeGrateDoor.cs
@@ -1,4 +1,6 @@
-﻿using SpiritReforged.Common.ItemCommon;
+﻿using SpiritReforged.Common;
+using SpiritReforged.Common.ItemCommon;
+using SpiritReforged.Common.TileCommon;
 using SpiritReforged.Common.TileCommon.PresetTiles;
 
 namespace SpiritReforged.Content.Ziggurat.Tiles;
@@ -6,4 +8,10 @@ namespace SpiritReforged.Content.Ziggurat.Tiles;
 public class BronzeGrateDoor : DoorTile
 {
 	public override IFurnitureData Info => new BasicInfo(this.AutoModItem(), AutoContent.ItemType<BronzePlating>());
+
+	public override void StaticDefaults()
+	{
+		SpiritSets.AllowsLiquid[Type] = true;
+		base.StaticDefaults();
+	}
 }
