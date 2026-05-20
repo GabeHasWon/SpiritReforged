09457978bfdd99b4932b6ed689e46ee9710d8d91
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2026-05-20 16:04:05 -0300

    Start boss patterns

--- Content/Forest/Glyphs/SealedKnight.cs
+++ Content/Forest/Glyphs/SealedKnight.cs
@@ -10,13 +10,34 @@ public class SealedKnight : ModNPC
 	public enum State
 	{
 		Inactive,
-		Idle
+		Active,
+		Leap,
+		Mortar,
+		Swing,
+		Block
 	}
+
+	private delegate void PatternDelegate(SealedKnight sealedKnight);
+
+	private static PatternDelegate[] _patterns;
 
 	public State CurrentState 
 	{ 
 		get => (State)NPC.ai[0];
 		set => NPC.ai[0] = (float)value;
+	}
+
+	public ref float PatternTime => ref NPC.ai[1];
+
+	public override void SetStaticDefaults()
+	{
+		_patterns = new PatternDelegate[Enum.GetValues<State>().Length];
+		_patterns[(int)State.Inactive] = Inactive;
+		_patterns[(int)State.Active] = Active;
+		_patterns[(int)State.Leap] = Leap;
+		_patterns[(int)State.Mortar] = Mortar;
+		_patterns[(int)State.Swing] = Swing;
+		_patterns[(int)State.Block] = Block;
 	}
 
 	public override void SetDefaults()
@@ -32,20 +53,9 @@ public class SealedKnight : ModNPC
 
 	public override void AI()
 	{
-		const int activation_range = 150;
 		NPC.direction = 1; //DEBUG
 
-		if (CurrentState is State.Inactive)
-		{
-			NPC.TargetClosest(false);
-
-			if (NPC.HasPlayerTarget && NPC.DistanceSQ(Main.player[NPC.target].Center) < activation_range * activation_range)
-				ChangeState(State.Idle);
-
-			return;
-		}
-
-		if (Main.rand.NextBool(3))
+		if (CurrentState != State.Inactive && Main.rand.NextBool(3))
 		{
 			var dust = Dust.NewDustPerfect(NPC.Top + Main.rand.NextVector2Circular(4, 4), Main.rand.NextFromList(DustID.Torch, DustID.Smoke), Vector2.UnitY * -Main.rand.NextFloat(1, 2));
 			dust.noGravity = true;
@@ -53,6 +63,9 @@ public class SealedKnight : ModNPC
 			if (dust.type == DustID.Smoke)
 				dust.alpha = 150;
 		}
+
+		_patterns[(int)CurrentState]?.Invoke(this); //Invoke the current pattern
+		PatternTime++;
 	}
 
 	public void ChangeState(State newState)
@@ -68,7 +81,7 @@ public class SealedKnight : ModNPC
 	{
 		if (!Main.dedServ && NPC.life <= 0)
 		{
-			for (int i = 1; i < 8; i++) //Spawn death gores
+			for (int i = 1; i < 10; i++) //Spawn death gores
 				Gore.NewGore(NPC.GetSource_Death(), NPC.position, NPC.velocity * 0.5f, Mod.Find<ModGore>("SealedKnight" + i).Type);
 		}
 	}
@@ -93,12 +106,14 @@ public class SealedKnight : ModNPC
 		Main.EntitySpriteDraw(texture, NPC.Bottom - screenPos + new Vector2(-11 * NPC.spriteDirection, NPC.gfxOffY + 2), source, NPC.DrawColor(drawColor), NPC.rotation, new(source.Width / 2, source.Height), NPC.scale, effects);
 
 		if (CurrentState is not State.Inactive)
-			DrawFlame(spriteBatch, screenPos);
+			DrawFlame(screenPos);
+
+		Utils.DrawBorderString(spriteBatch, CurrentState.ToString(), NPC.Top - screenPos, Main.MouseTextColorReal, 1, 0.5f, 0.5f); //DEBUG
 
 		return false;
 	}
 
-	private void DrawFlame(SpriteBatch spriteBatch, Vector2 screenPos)
+	private void DrawFlame(Vector2 screenPos)
 	{
 		Texture2D texture = TextureAssets.Flames[0].Value;
 		Rectangle source = new(22, 0, 22, 22);
@@ -109,4 +124,44 @@ public class SealedKnight : ModNPC
 			Main.EntitySpriteDraw(texture, position, source, NPC.DrawColor(Color.White.Additive()), NPC.rotation, source.Size() / 2, NPC.scale, 0);
 		}
 	}
+
+	#region patterns
+	private static void Inactive(SealedKnight sealedKnight)
+	{
+		const int activation_range = 150;
+
+		NPC npc = sealedKnight.NPC;
+		npc.TargetClosest(false);
+
+		if (npc.HasPlayerTarget && npc.DistanceSQ(Main.player[npc.target].Center) < activation_range * activation_range)
+			sealedKnight.ChangeState(State.Active);
+	}
+
+	private static void Active(SealedKnight sealedKnight)
+	{
+
+	}
+
+	private static void Mortar(SealedKnight sealedKnight)
+	{
+		if (sealedKnight.PatternTime >= 30)
+			sealedKnight.ChangeState(State.Active);
+	}
+
+	private static void Leap(SealedKnight sealedKnight)
+	{
+
+	}
+
+	private static void Swing(SealedKnight sealedKnight)
+	{
+
+	}
+
+	private static void Block(SealedKnight sealedKnight)
+	{
+		if (sealedKnight.PatternTime >= 180)
+			sealedKnight.ChangeState(State.Active);
+	}
+	#endregion
 }
