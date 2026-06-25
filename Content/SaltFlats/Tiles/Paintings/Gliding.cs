ab5c55ce2c031b23b567a30f444a06d2ebd2ae02
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2026-06-25 20:33:48 -0300

    Merge fix

--- Content/SaltFlats/Tiles/Paintings/Gliding.cs
+++ Content/SaltFlats/Tiles/Paintings/Gliding.cs
@@ -14,7 +14,7 @@ public class Gliding : PaintingTile
 	public override void SetStaticDefaults()
 	{
 		base.SetStaticDefaults();
-		NPCShopHelper.AddEntry(new NPCShopHelper.ConditionalEntry((shop) => shop.NpcType == NPCID.Painter, new NPCShop.Entry(Type, Condition.TimeDay, SpiritConditions.InSaltFlats)));
+		NPCShopHelper.AddEntry(new NPCShopHelper.ConditionalEntry((shop) => shop.NpcType == NPCID.Painter, new NPCShop.Entry(this.AutoItemType(), Condition.TimeDay, SpiritConditions.InSaltFlats)));
 	}
 
 	public override void AddItemRecipes(ModItem item) 
