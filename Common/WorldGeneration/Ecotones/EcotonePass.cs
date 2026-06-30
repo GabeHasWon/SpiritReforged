cbbb479f1917e1c2591a34bc144e08552b632380
GabeHasWon <gabehw.contact@gmail.com>
2026-06-30 15:21:44 -0500

    publicize & move EcotonePass

---
+++ Common/WorldGeneration/Ecotones/EcotonePass.cs
@@ -0,0 +1,11 @@
+﻿using Terraria.GameContent.Generation;
+
+namespace SpiritReforged.Common.WorldGeneration.Ecotones;
+
+/// <summary>
+/// Defines a pass as one that places an ecotone. This is solely a marker for use in the manual ecotone selector.
+/// </summary>
+public class EcotonePass(string name, WorldGenLegacyMethod method, EcotoneBase ecotone) : PassLegacy(name, method)
+{
+	public readonly EcotoneBase Ecotone = ecotone;
+}
