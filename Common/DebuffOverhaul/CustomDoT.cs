64e80847abf0255a61675bfc3289187a93acf855
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2026-08-05 15:58:28 -0300

    AddCustomDoT call

--- Common/DebuffOverhaul/CustomDoT.cs
+++ Common/DebuffOverhaul/CustomDoT.cs
@@ -3,7 +3,46 @@
 /// <summary> A damage-over-time buff extension that can be registered through mod call. </summary>
 public class CustomDoT(float scalability, int damageLimit, bool stackable, Action scalingBehaviour = null, Action<SpriteBatch, NPC, Color, Vector2, float, float> onPostDraw = null) : DoTExtension
 {
-    public override BuffSettings Settings => _settings;
+	public static bool AddCustomDoT(object[] args)
+	{
+		if (args.Length < 6)
+			throw new ArgumentException("AddCustomDoT requires at least 6 parameters.");
+
+		if (args[0] is not int)
+			throw new ArgumentException("AddCustomDoT parameter 1 should be an int.");
+
+		int buffType = (int)args[0];
+
+		if (args[1] is not float)
+			throw new ArgumentException("AddCustomDoT parameter 2 should be a float.");
+
+		float scalability = (float)args[1];
+
+		if (args[2] is not int)
+			throw new ArgumentException("AddCustomDoT parameter 3 should be an int.");
+
+		int damageLimit = (int)args[2];
+
+		if (args[3] is not bool)
+			throw new ArgumentException("AddCustomDoT parameter 4 should be a bool.");
+
+		bool stackable = (bool)args[3];
+
+		if (args[4] is not Action or null)
+			throw new ArgumentException("AddCustomDoT parameter 5 should be an Action or null.");
+
+		var scalingBehaviour = (Action)args[4];
+
+		if (args[5] is not Action<SpriteBatch, NPC, Color, Vector2, float, float> or null)
+			throw new ArgumentException("AddCustomDoT parameter 6 should be an Action<SpriteBatch, NPC, Color, Vector2, float, float> or null.");
+
+		var onPostDraw = (Action<SpriteBatch, NPC, Color, Vector2, float, float>)args[5];
+
+		BuffHandler.Register(new CustomDoT(scalability, damageLimit, stackable, scalingBehaviour, onPostDraw), buffType);
+		return true;
+	}
+
+	public override BuffSettings Settings => _settings;
 
     private readonly BuffSettings _settings = new(scalability, damageLimit, stackable, scalingBehaviour);
     private readonly Action<SpriteBatch, NPC, Color, Vector2, float, float> _onPostDraw = onPostDraw;
