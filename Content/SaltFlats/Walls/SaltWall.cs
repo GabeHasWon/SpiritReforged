a3db4f0781a781a475d971b0b45b9a6b2b02d893
GabeHasWon <gabehw.contact@gmail.com>
2026-08-08 14:29:08 -0500

    gen config folder load, drywood chair fixes and explicit interface implementation consistnency on salt walls

--- Content/SaltFlats/Walls/SaltWall.cs
+++ Content/SaltFlats/Walls/SaltWall.cs
@@ -9,7 +9,7 @@ public class SaltWall : ModWall, IAutoloadUnsafeWall, ILoadItem
 {
 	public static int UnsafeType { get; private set; } = SpiritReforgedMod.Instance.Find<ModWall>("SaltWallUnsafe").Type;
 
-	public void AddItemRecipes(ModItem item)
+	void ILoadItem.AddItemRecipes(ModItem item)
 	{
 		int salt = AutoContent.ItemType<SaltBlockDull>();
 		int wall = AutoContent.ItemType<SaltWall>();
