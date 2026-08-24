abe7233c42ff586cdedf314badcb5d2cb03a3534
GabeHasWon <gabehw.contact@gmail.com>
2026-08-24 00:24:31 -0500

    mini tunnels

--- Content/Basalt/BasaltBiome.cs
+++ Content/Basalt/BasaltBiome.cs
@@ -1,5 +1,6 @@
 ﻿using SpiritReforged.Common.WorldGeneration.Microbiomes;
 using SpiritReforged.Content.Basalt.Tiles;
+using Terraria;
 using Terraria.DataStructures;
 
 namespace SpiritReforged.Content.Basalt;
@@ -14,18 +15,18 @@ internal class BasaltBiome : Microbiome
 			point = PlaceMainTunnel(point);
 	}
 
-	private static Point16 PlaceMainTunnel(Point16 point)
+	private static Point16 PlaceMainTunnel(Point16 point, float heightMod = 1f, float? forceAngle = null)
 	{
 		const float AngleRange = 1f;
 
 		ushort wallType = WallID.WhiteDynasty;
 		int width = WorldGen.genRand.Next(80, 110);
-		int height = WorldGen.genRand.Next(60, 95);
-		float angle = WorldGen.genRand.NextFloat(-AngleRange, AngleRange);
+		int height = (int)(WorldGen.genRand.Next(60, 95) * heightMod);
+		float angle = forceAngle ?? WorldGen.genRand.NextFloat(-AngleRange, AngleRange);
 		Vector2 position = point.ToVector2();
-
-		float[] offsetY = GetRandomizedHeights(width, 0, 8, false);
-		float[] heights = GetRandomizedHeights(width, 3, 6, true);
+		bool isReservoir = forceAngle is not null;
+		float[] offsetY = GetRandomizedHeights(width, 0, 8, false, heightMod);
+		float[] heights = GetRandomizedHeights(width, 3, 6, true, heightMod);
 
 		for (float x = width / -2f; x < width / 2f; ++x)
 		{
@@ -49,19 +50,36 @@ internal class BasaltBiome : Microbiome
 				if (Math.Abs(y) <= 1f)
 					tile.TileColor = PaintID.WhitePaint;
 
-				if (Math.Abs(y) <= Math.Abs(heights[indexer]) + 8)
+				if (Math.Abs(y) <= (Math.Abs(heights[indexer]) + 8) *heightMod)
 				{
 					tile.HasTile = false;
 					tile.WallType = wallType;
+
+					if (isReservoir)
+					{
+						tile.LiquidAmount = 255;
+						tile.LiquidType = LiquidID.Water;
+					}
 				}
 			}
 		}
 
-		var newPos = (new Vector2(Main.rand.NextFloat(width / -3f, width / 3f) * 0.95f, 0).RotatedBy(angle) + position).ToPoint16();
+		if (!isReservoir && WorldGen.genRand.NextBool(3))
+		{
+			int dir = WorldGen.genRand.NextBool() ? -1 : 1;
+			var heightOff = new Vector2(WorldGen.genRand.NextFloat(height / 4f, height / 5f) * 0.95f * dir, 0).RotatedBy(angle + MathHelper.PiOver2);
+			var tunnel = (BaseAngle(width).RotatedBy(angle) + position + heightOff).ToPoint16();
+
+			PlaceMainTunnel(tunnel, WorldGen.genRand.NextFloat(0.15f, 0.25f), angle);
+		}
+
+		var newPos = (BaseAngle(width).RotatedBy(angle) + position).ToPoint16();
 		return newPos;
+
+		static Vector2 BaseAngle(int width) => new(WorldGen.genRand.NextFloat(width / -3f, width / 3f) * 0.95f, 0);
 	}
 
-	private static float[] GetRandomizedHeights(int width, int rangeMin, int rangeMax, bool abs)
+	private static float[] GetRandomizedHeights(int width, int rangeMin, int rangeMax, bool abs, float varianceAdjustment)
 	{
 		float[] offsetY = new float[width];
 		int stepLength = WorldGen.genRand.Next(4, 11);
@@ -75,15 +93,15 @@ internal class BasaltBiome : Microbiome
 			if (stepLength == 0)
 			{
 				if (WorldGen.genRand.NextBool(6))
-					currentOffset = WorldGen.genRand.Next(-8, 9);
+					currentOffset = (int)(WorldGen.genRand.Next(-8, 9) * varianceAdjustment);
 				else
 				{
 					if (currentOffset < -4)
-						currentOffset += WorldGen.genRand.Next(1, 8);
+						currentOffset += (int)(WorldGen.genRand.Next(1, 8) * varianceAdjustment);
 					else if (currentOffset > 4)
-						currentOffset -= WorldGen.genRand.Next(1, 8);
+						currentOffset -= (int)(WorldGen.genRand.Next(1, 8) * varianceAdjustment);
 					else
-						currentOffset += WorldGen.genRand.Next(-4, 4);
+						currentOffset += (int)(WorldGen.genRand.Next(-4, 4) * varianceAdjustment);
 				}
 
 				stepLength = WorldGen.genRand.Next(4, 11);
