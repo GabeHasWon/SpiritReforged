f5963e4ba744db7921052f359ebe19f7a7dc225d
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2026-08-24 23:53:51 -0300

    Jellyfish Jam Jar

---
+++ Common/ItemCommon/IDrawHeld.cs
@@ -0,0 +1,33 @@
+﻿using Terraria.DataStructures;
+
+namespace SpiritReforged.Common.ItemCommon;
+
+public interface IDrawHeld
+{
+	public sealed class CustomHeldLayer : PlayerDrawLayer
+	{
+		public override Position GetDefaultPosition() => new BeforeParent(PlayerDrawLayers.HeldItem);
+
+		protected override void Draw(ref PlayerDrawSet drawInfo)
+		{
+			if (drawInfo.drawPlayer.HeldItem.ModItem is IDrawHeld iHeld)
+				iHeld.DrawHeld(ref drawInfo);
+		}
+	}
+
+	public void DrawHeld(ref PlayerDrawSet drawinfo);
+
+	public static void DrawSwordHeld(ref PlayerDrawSet drawinfo, Texture2D texture, Rectangle? source = null)
+	{
+		source ??= texture.Frame();
+
+		Vector2 bobOffset = Main.OffsetsPlayerHeadgear[drawinfo.drawPlayer.bodyFrame.Y / drawinfo.drawPlayer.bodyFrame.Height] * drawinfo.drawPlayer.gravDir;
+		Vector2 center = drawinfo.drawPlayer.MountedCenter + bobOffset;
+		Vector2 drawPos = new((int)(center.X - Main.screenPosition.X), (int)(center.Y + 6 * drawinfo.drawPlayer.gravDir - Main.screenPosition.Y + drawinfo.drawPlayer.gfxOffY));
+
+		float rotation = -0.15f * drawinfo.drawPlayer.direction + drawinfo.drawPlayer.fullRotation + MathHelper.Pi;
+		Color color = Lighting.GetColor((int)drawinfo.drawPlayer.Center.X / 16, (int)drawinfo.drawPlayer.Center.Y / 16);
+
+		drawinfo.DrawDataCache.Add(new DrawData(texture, drawPos, source, color, rotation, source.Value.Size() / 2, 1, drawinfo.playerEffect, 0));
+	}
+}
--- Content/Ocean/Items/JellyMinion/JellyfishJamJar.cs
+++ Content/Ocean/Items/JellyMinion/JellyfishJamJar.cs
@@ -8,10 +8,11 @@ using Terraria.Audio;
 using Terraria.DataStructures;
 using Terraria.GameContent.ItemDropRules;
 
-namespace SpiritReforged.Content.Ocean.Items.JellyfishStaff;
+namespace SpiritReforged.Content.Ocean.Items.JellyMinion;
 
-[AutoloadGlowmask("255, 255, 255")]
-public class JellyfishStaff : ModItem
+[LegacyName("JellyfishStaff")]
+[AutoloadGlowmask("255,255,255")]
+public class JellyfishJamJar : ModItem, IDrawHeld
 {
 	public override void SetStaticDefaults()
 	{
@@ -23,6 +24,8 @@ public class JellyfishStaff : ModItem
 
 		MoRHelper.AddElement(Item, MoRHelper.Thunder);
 		MoRHelper.AddElement(Item, MoRHelper.Water, true);
+
+		Main.RegisterItemAnimation(Type, new DrawAnimationVertical(2, 2) { NotActuallyAnimating = true });
 	}
 
 	public override void SetDefaults()
@@ -34,17 +37,18 @@ public class JellyfishStaff : ModItem
 		Item.mana = 10;
 		Item.damage = 14;
 		Item.knockBack = 2.5f;
-		Item.useStyle = ItemUseStyleID.Swing;
+		Item.useStyle = ItemUseStyleID.Rapier;
 		Item.useTime = 30;
 		Item.useAnimation = 30;
 		Item.DamageType = DamageClass.Summon;
 		Item.noMelee = true;
+		Item.noUseGraphic = true;
 		Item.shoot = ModContent.ProjectileType<JellyfishMinion>();
+		Item.shootSpeed = 4;
 		Item.UseSound = SoundID.Item44;
 		Item.autoReuse = true;
 	}
 
-	public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback) => position = Main.MouseWorld;
 	public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
 	{
 		SoundEngine.PlaySound(SoundID.SplashWeak with { PitchVariance = 0.3f }, position);
@@ -52,11 +56,49 @@ public class JellyfishStaff : ModItem
 		for (int i = 0; i < 3; i++)
 		{
 			ParticleHandler.SpawnParticle(new BubbleParticle(position + Main.rand.NextVector2Circular(15f, 15f), Main.rand.NextVector2Circular(2.5f, 2.5f), Main.rand.NextFloat(0.12f, 0.26f), Main.rand.Next(20, 40)));
-
 			Dust.NewDustPerfect(position + Main.rand.NextVector2Circular(15f, 15f), DustID.Water, Main.rand.NextVector2Circular(5f, 5f), 55, default, 0.7f).noGravity = true;
 		}
 
 		return true;
 	}
-	public override void Update(ref float gravity, ref float maxFallSpeed) => Lighting.AddLight(Item.position, .224f * 2, .133f * 2, .255f * 2);
+
+	public override void Update(ref float gravity, ref float maxFallSpeed) => Lighting.AddLight(Item.position, 0.224f * 2, 0.133f * 2, 0.255f * 2);
+
+	void IDrawHeld.DrawHeld(ref PlayerDrawSet drawinfo)
+	{
+		Player player = drawinfo.drawPlayer;
+		if (!player.ItemAnimationActive)
+			return;
+
+		Texture2D texture = TextureAssets.Item[Type].Value;
+		Rectangle source = texture.Frame(1, 2, 0, 1, 0, -2);
+
+		float progress = (float)player.itemAnimation / player.itemAnimationMax;
+		Vector2 offhand = GetOffhand(player, out _) + new Vector2((10 - (int)(progress * 4 / 2) * 2) * player.direction, 4);
+		Vector2 halfSize = player.Size / 2;
+
+		float armRotation = drawinfo.compositeFrontArmRotation + MathHelper.PiOver2;
+
+		if (player.direction == -1)
+			armRotation += MathHelper.Pi;
+
+		Vector2 position = drawinfo.Position + halfSize + (offhand - halfSize).RotatedBy(armRotation) - Main.screenPosition;
+		float rotation = MathHelper.PiOver4 * player.direction;
+		Color color = Lighting.GetColor((int)player.Center.X / 16, (int)player.Center.Y / 16);
+
+		drawinfo.DrawDataCache.Add(new DrawData(texture, position, source, color, rotation, source.Size() / 2, 1, drawinfo.playerEffect, 0));
+
+		static Vector2 GetOffhand(Player player, out int frame)
+		{
+			Vector2 offhand = Main.OffsetsPlayerOffhand[frame = player.bodyFrame.Y / player.bodyFrame.Height];
+
+			if (player.direction != 1)
+				offhand.X = player.width - offhand.X;
+
+			if (player.gravDir != 1f)
+				offhand.Y -= player.height;
+
+			return offhand;
+		}
+	}
 }
