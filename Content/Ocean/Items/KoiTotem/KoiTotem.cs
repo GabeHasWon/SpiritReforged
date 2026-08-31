b65cf6ba30fdaf95ca763806bec455f868418b5f
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2026-08-30 21:38:16 -0300

    Cleanup cleanup cleanup

--- Content/Ocean/Items/KoiTotem/KoiTotem.cs
+++ Content/Ocean/Items/KoiTotem/KoiTotem.cs
@@ -3,6 +3,8 @@ using SpiritReforged.Common.Particle;
 using SpiritReforged.Common.TileCommon;
 using SpiritReforged.Content.Particles;
 using Terraria.Audio;
+using Terraria.GameContent.Drawing;
+using TileHelper.Common;
 
 namespace SpiritReforged.Content.Ocean.Items.KoiTotem;
 
@@ -24,6 +26,8 @@ public class KoiTotem : FloatingItem
 
 public class KoiTotemTile : ModTile
 {
+	public static readonly SoundStyle Feedback = new("SpiritReforged/Assets/SFX/Ambient/MagicFeedback", 2);
+
 	public override void SetStaticDefaults()
 	{
 		Main.tileFrameImportant[Type] = true;
@@ -50,7 +54,7 @@ public class KoiTotemTile : ModTile
 
 	public override void NearbyEffects(int i, int j, bool closer)
 	{
-		var player = Main.LocalPlayer;
+		Player player = Main.LocalPlayer;
 
 		if (!closer)
 		{
@@ -59,49 +63,51 @@ public class KoiTotemTile : ModTile
 		}
 		else if (TileObjectData.IsTopLeft(i, j) && KoiTotemBuff.CursorOpacity > 0) //Create fancy visuals when bait is replenished
 		{
-			var t = Main.tile[i, j];
-			int height = TileObjectData.GetTileData(t)?.Height ?? 0;
-			var pos = new Vector2(i, j).ToWorldCoordinates(0, height * 16);
+			int height = TileObjectData.GetTileData(Main.tile[i, j])?.Height ?? 0;
+			Vector2 position = new Vector2(i, j).ToWorldCoordinates(0, height * 16);
 
 			if (Main.rand.NextBool())
 			{
 				var color = Color.Lerp(Color.LightBlue, Color.Cyan, Main.rand.NextFloat());
 				float magnitude = Main.rand.NextFloat();
 
-				ParticleHandler.SpawnParticle(new GlowParticle(pos + new Vector2(Main.rand.NextFloat(32), 0), Vector2.UnitY * -magnitude, color, (1f - magnitude) * .25f, Main.rand.Next(30, 120), 5, extraUpdateAction: delegate (Particle p)
-					{ p.Velocity = p.Velocity.RotatedBy(Main.rand.NextFloat(-.1f, .1f)); }));
+				ParticleHandler.SpawnParticle(new GlowParticle(position + new Vector2(Main.rand.NextFloat(32), 0), Vector2.UnitY * -magnitude, color, (1f - magnitude) * .25f, Main.rand.Next(30, 120), 5, extraUpdateAction: delegate (Particle p)
+					{ p.Velocity = p.Velocity.RotatedBy(Main.rand.NextFloat(-0.1f, 0.1f)); }));
 			}
 
-			if (KoiTotemBuff.CursorOpacity > .9f)
+			if (KoiTotemBuff.CursorOpacity > 0.9f)
 			{
-				const string path = "SpiritReforged/Assets/SFX/Ambient/MagicFeedback";
+				ParticleHandler.SpawnParticle(new DissipatingImage(position + new Vector2(18, 0), Color.Cyan * 0.15f, 0, 0.25f, 1f, "Bloom", 120));
 
-				ParticleHandler.SpawnParticle(new DissipatingImage(pos + new Vector2(18, 0), Color.Cyan * .15f, 0, .25f, 1f, "Bloom", 120));
-
-				SoundEngine.PlaySound(new SoundStyle(path + 1) with { Volume = .3f, PitchRange = (-1, -.75f) }, pos);
-				SoundEngine.PlaySound(new SoundStyle(path + 2) with { Volume = .4f, PitchRange = (-.65f, -.35f) }, pos);
+				SoundEngine.PlaySound(Feedback with { Volume = 0.3f, PitchRange = (-1, -0.75f) }, position);
+				SoundEngine.PlaySound(Feedback with { Volume = 0.4f, PitchRange = (-0.65f, -0.35f) }, position);
 			}
 		}
 	}
 
 	public override bool PreDraw(int i, int j, SpriteBatch spriteBatch)
 	{
-		if (!TileMethods.GetVisualInfo(i, j, out var color, out var texture))
+		Tile tile = Main.tile[i, j];
+
+		if (!TileDrawing.IsVisible(tile) || TileObjectData.GetTileData(tile) is not TileObjectData tileObjectData)
 			return false;
 
-		var t = Main.tile[i, j];
-		var frame = new Point(t.TileFrameX, t.TileFrameY);
-		var source = new Rectangle(frame.X, frame.Y, 18, (frame.Y == 54) ? 18 : 16);
-		int offX = (frame.X % TileObjectData.GetTileData(Type, 0).CoordinateFullWidth == 0) ? -2 : 0;
-		var position = new Vector2(i, j) * 16 - Main.screenPosition + TileMethods.TileOffset + new Vector2(offX, 0);
+		int offset = (tile.TileFrameX % tileObjectData.CoordinateFullWidth == 0) ? -2 : 0;
+		Rectangle source = new(tile.TileFrameX, tile.TileFrameY, 18, (tile.TileFrameY == 54) ? 18 : 16);
+		Vector2 position = new Vector2(i, j) * 16 - Main.screenPosition + TileMethods.TileOffset + new Vector2(offset, 0);
 
-		spriteBatch.Draw(texture, position, source, color, 0, Vector2.Zero, 1, SpriteEffects.None, 0);
-
+		spriteBatch.Draw(Helpers.GetTileTextureValue(tile), position, source, Lighting.GetColor(i, j), 0, Vector2.Zero, 1, 0, 0);
 		return false;
 	}
 
 	public override bool PreDrawPlacementPreview(int i, int j, SpriteBatch spriteBatch, ref Rectangle frame, ref Vector2 position, ref Color color, bool validPlacement, ref SpriteEffects spriteEffects)
 	{
+		if (TileObjectData.GetTileData(Type, 0) is TileObjectData tileObjectData)
+		{
+			int offset = (frame.X % tileObjectData.CoordinateFullWidth == 0) ? -2 : 0;
+			position += Vector2.UnitX * (offset + 1);
+		}
+
 		return true;
 	}
 }
--- Content/Ocean/Tiles/BeachUmbrella.cs
+++ Content/Ocean/Tiles/BeachUmbrella.cs
@@ -5,8 +5,11 @@ using TileHelper.Common;
 
 namespace SpiritReforged.Content.Ocean.Tiles;
 
-public class BeachUmbrella : ModTile, WindTileRenderer.IDrawInWind, ILoadItem, IModifySmartTarget
+public class BeachUmbrella : ModTile, ILoadItem, IModifySmartTarget
 {
+	private const int WIDTH = 4;
+	private const int HEIGHT = 5;
+
 	public void SetItemDefaults(ModItem item) => item.Item.value = Item.buyPrice(silver: 20);
 
 	public override void SetStaticDefaults()
@@ -37,36 +40,42 @@ public class BeachUmbrella : ModTile, WindTileRenderer.IDrawInWind, ILoadItem, I
 		DustType = -1;
 	}
 
-	void WindTileRenderer.IDrawInWind.DrawInWind(SpriteBatch spriteBatch, int i, int j, float rotation, Vector2 position, Vector2 origin)
+	public override bool PreDraw(int i, int j, SpriteBatch spriteBatch)
 	{
-		const int width = 4;
-		const int height = 5;
+		Tile tile = Main.tile[i, j];
+		if (TileDrawing.IsVisible(tile) && TileObjectData.IsTopLeft(i, j))
+			Main.instance.TilesRenderer.AddSpecialPoint(i, j, TileDrawing.TileCounterType.CustomNonSolid);
 
+		return false;
+	}
+
+	public override void SpecialDraw(int i, int j, SpriteBatch spriteBatch)
+	{
 		Tile tile = Main.tile[i, j];
 		if (TileDrawing.IsVisible(tile) && TileObjectData.IsTopLeft(i, j) && TileObjectData.GetTileData(Main.tile[i, j]) is TileObjectData tileObjectData)
 		{
 			Texture2D texture = Helpers.GetTileTextureValue(tile);
-			float physics = (this as WindTileRenderer.IDrawInWind).GetWindStrength(i, j);
+			float physics = GetPhysics(i, j);
 			bool flipped = tile.TileFrameX == 18;
 
-			for (int x = i; x < i + width; x++)
+			for (int x = i; x < i + WIDTH; x++)
 			{
-				for (int y = j; y < j + height; y++)
+				for (int y = j; y < j + HEIGHT; y++)
 				{
 					(int gridX, int gridY) = (x - i, y - j);
-					Rectangle source = new(gridX * 18 + (flipped ? (18 * width) : 0), gridY * 18, 16, 18);
+					Rectangle source = new(gridX * 18 + (flipped ? (18 * WIDTH) : 0), gridY * 18, 16, 18);
 
-					rotation = (1.5f - gridY / (height - 1f)) * physics * 0.1f;
-					position = new Vector2(x, y) * 16 - Main.screenPosition + new Vector2(0, Math.Abs(rotation) * 20f);
-					origin = (tileObjectData.Origin.ToVector2() + Vector2.One * 2.5f - new Vector2(gridX, gridY)) * 16;
+					float rotation = (1.5f - gridY / (HEIGHT - 1f)) * physics * 0.1f;
+					Vector2 position = new Vector2(x, y) * 16 - Main.screenPosition + new Vector2(0, Math.Abs(rotation) * 20f);
+					Vector2 origin = (tileObjectData.Origin.ToVector2() + Vector2.One * 2.5f - new Vector2(gridX, gridY)) * 16;
 
-					spriteBatch.Draw(texture, position + origin - new Vector2(16 * 2), source, Lighting.GetColor(x, y), rotation, origin, 1, SpriteEffects.None, 0);
+					spriteBatch.Draw(texture, position + origin - new Vector2(16 * (flipped ? 1 : 2), 16 * 2), source, Lighting.GetColor(x, y), rotation, origin, 1, SpriteEffects.None, 0);
 				}
 			}
 		}
 	}
 
-	float WindTileRenderer.IDrawInWind.GetWindStrength(int i, int j)
+	private static float GetPhysics(int i, int j)
 	{
 		if (TileObjectData.GetTileData(Framing.GetTileSafely(i, j)) is TileObjectData tileObjectData)
 		{
@@ -79,24 +88,21 @@ public class BeachUmbrella : ModTile, WindTileRenderer.IDrawInWind, ILoadItem, I
 
 	public override bool PreDrawPlacementPreview(int i, int j, SpriteBatch spriteBatch, ref Rectangle frame, ref Vector2 position, ref Color color, bool validPlacement, ref SpriteEffects spriteEffects)
 	{
-		const int width = 4;
-		const int height = 5;
-
 		if (frame.Y == 0)
 		{
 			Texture2D texture = TextureAssets.Tile[Type].Value;
 			bool flipped = frame.X == 18;
 
-			for (int x = i; x < i + width; x++)
+			for (int x = i; x < i + WIDTH; x++)
 			{
-				for (int y = j; y < j + height; y++)
+				for (int y = j; y < j + HEIGHT; y++)
 				{
 					(int gridX, int gridY) = (x - i, y - j);
 
 					position = new Vector2(x, y) * 16f - Main.screenPosition + (Main.drawToScreen ? Vector2.Zero : new Vector2(Main.offScreenRange));
-					Rectangle source = new(gridX * 18 + (flipped ? (18 * width) : 0), gridY * 18, 16, 18);
+					Rectangle source = new(gridX * 18 + (flipped ? (18 * WIDTH) : 0), gridY * 18, 16, 18);
 
-					spriteBatch.Draw(texture, position - new Vector2(16 * 2), source, color, 0, Vector2.Zero, 1, spriteEffects, 0);
+					spriteBatch.Draw(texture, position - new Vector2(16 * (flipped ? 1 : 2), 16 * 2), source, color, 0, Vector2.Zero, 1, spriteEffects, 0);
 				}
 			}
 		}
