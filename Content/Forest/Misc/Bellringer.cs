92f96e1912304e480b30ebb45ed0233eba6f7ca6
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2026-09-08 16:28:47 -0300

    Bellringer polish

--- Content/Forest/Misc/Bellringer.cs
+++ Content/Forest/Misc/Bellringer.cs
@@ -15,7 +15,10 @@ public class Bellringer : ClubItem
 {
 	public class BellringerProj : BaseClubProj
 	{
-		private Vector2 _bellVelocity;
+		public static readonly SoundStyle Bell = new("SpiritReforged/Assets/SFX/Item/BellGong");
+
+		public const int SHOCKWAVE_SCALE = 250;
+		private float _bellRotation;
 
 		public override float WindupTimeRatio => 0.8f;
 
@@ -46,13 +49,22 @@ public class Bellringer : ClubItem
 
 			SwingTrailParameters parameters = new(AngleRange * angleRangeMod, -HoldAngle_Final + rotOffset, trailDist, trailWidth)
 			{
-				Color = Color.White,
-				SecondaryColor = Color.SandyBrown,
-				TrailLength = 0.33f,
-				Intensity = 0.75f,
+				Color = new Color(90, 70, 40),
+				SecondaryColor = Color.Black * 0.8f,
+				TrailLength = 0.2f,
+				Intensity = 1,
 			};
 
 			renderer.CreateTrail(Projectile, new SwingTrail(Projectile, parameters, GetSwingProgressStatic, SwingTrail.BasicSwingShaderParams));
+			renderer.CreateTrail(Projectile, new SwingTrail(Projectile, parameters with { Distance = trailDist + 30, Width = trailWidth + 10 }, GetSwingProgressStatic, SwingTrail.BasicSwingShaderParams));
+		}
+
+		public override void SafeAI()
+		{
+			float delta = Projectile.rotation - Projectile.oldRot[1];
+
+			_bellRotation = _bellRotation + delta * 1.5f - Owner.velocity.X / 150f + Owner.velocity.Y / 100f * Owner.direction;
+			_bellRotation = Utils.AngleLerp(_bellRotation, Projectile.rotation, 0.1f);
 		}
 
 		public override void OnSmash(Vector2 position)
@@ -73,24 +85,44 @@ public class Bellringer : ClubItem
 
 					ParticleHandler.SpawnParticle(new ImpactLine(position, velocity, Color.Goldenrod.Additive(), new Vector2(1, height) * scale, duration));
 					ParticleHandler.SpawnParticle(new ImpactLine(position, velocity, Color.White.Additive(), new Vector2(1, height) * 0.5f * scale, duration));
+
+					Vector2 emberVelocity = Main.rand.NextVector2Circular(5, 5) * scale;
+					ParticleHandler.SpawnParticle(new EmberParticle(position, emberVelocity, Color.White.Additive(100), Color.Brown.Additive(100), 0.5f, duration * 2, 2));
 				}
 
-				ParticleHandler.SpawnParticle(new PulseCircle(position, Color.Goldenrod.Additive(), Color.PaleVioletRed.Additive(), 0.12f, scale * 400, duration));
-				ParticleHandler.SpawnParticle(new PulseCircle(position, Color.White.Additive(), 0.05f, scale * 400, duration));
-			}
+				float scaleModifier = (SHOCKWAVE_SCALE + 10) * 2;
+				ParticleHandler.SpawnParticle(new PulseCircle(position, Color.Goldenrod.Additive() * 0.3f, 0.12f, scale * scaleModifier, duration));
+				ParticleHandler.SpawnParticle(new PulseCircle(position, Color.White.Additive(), 0.03f, scale * scaleModifier, duration));
 
-			SoundEngine.PlaySound(SoundID.DD2_SkeletonHurt with { Pitch = 0.5f }, Projectile.Center);
-			_bellVelocity += Projectile.velocity * 20;
+				ParticleHandler.SpawnParticle(new PulseCircle(position, Color.PaleVioletRed.Additive() * 0.2f, 0.05f, scale * scaleModifier, duration + 10));
+
+				for (int i = 0; i < (FullCharge ? 6 : 2); i++)
+				{
+					ParticleHandler.SpawnParticle(new CompositeSmoke(position + Main.rand.NextVector2Circular(30, 30), -Vector2.UnitY, Color.SaddleBrown, 60, false, false));
+					ParticleHandler.SpawnParticle(new SmallCompositeSmoke(position + Main.rand.NextVector2Circular(30, 30), -Vector2.UnitY, Color.SandyBrown, 60, false, false));
+				}
+
+				SoundEngine.PlaySound(SoundID.DD2_SkeletonHurt with { Pitch = 0.5f }, Projectile.Center);
+				SoundEngine.PlaySound(Bell with { PitchVariance = 0.2f, Volume = Charge }, Projectile.Center);
+			}
 		}
 
-		public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
-		{
-			var position = Vector2.Lerp(Projectile.Center, target.Center, 0.6f);
+		public override bool? CanDamage() => CheckAIState(AIStates.CHARGING) ? false : null;
 
-			for (int i = 0; i < (FullCharge ? 3 : 1); i++)
+		public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox)
+		{
+			if (CheckAIState(AIStates.POST_SMASH))
 			{
-				ParticleHandler.SpawnParticle(new CompositeSmoke(position + Main.rand.NextVector2Circular(30, 30), -Vector2.UnitY, Color.SaddleBrown, 60, false, false));
-				ParticleHandler.SpawnParticle(new SmallCompositeSmoke(position + Main.rand.NextVector2Circular(30, 30), -Vector2.UnitY, Color.SandyBrown, 60, false, false));
+				Vector2 center = projHitbox.Center();
+				int scale = (int)(SHOCKWAVE_SCALE * Charge);
+
+				Rectangle inflatedArea = new((int)center.X - scale / 2, (int)center.Y - scale / 2, scale, scale);
+
+				return targetHitbox.Intersects(inflatedArea);
+			}
+			else
+			{
+				return base.Colliding(projHitbox, targetHitbox);
 			}
 		}
 
@@ -100,29 +132,33 @@ public class Bellringer : ClubItem
 			if (AllowedAftertrailDraw(ref trailOpacity))
 				DrawAftertrail(texture, lightColor * trailOpacity, drawPosition);
 
+			Texture2D white = TextureColorCache.ColorSolid(texture, Color.White);
 			Rectangle poleSource = texture.Frame(1, Main.projFrames[Type], 0, 0, 0, -2);
 			Rectangle bellSource = new(56, 120, 62, 62);
 
-			float bellRotation = (_bellVelocity.X + _bellVelocity.Y) / 15f;
+			float bellRotation = _bellRotation;
 			Vector2 bellOrigin = (Effects == SpriteEffects.FlipHorizontally) ? new Vector2(bellSource.Width, bellSource.Height) : new Vector2(0, bellSource.Height);
 			Vector2 bellPosition = handPosition + (new Vector2(46, -46) * TotalScale).RotatedBy((Effects == SpriteEffects.FlipHorizontally) ? Projectile.rotation - MathHelper.PiOver2 : Projectile.rotation) - Main.screenPosition;
 
-			Main.EntitySpriteDraw(texture, bellPosition, bellSource, Projectile.GetAlpha(lightColor), Projectile.rotation + bellRotation, bellOrigin, TotalScale, Effects, 0);
+			if (!CheckAIState(AIStates.POST_SMASH))
+			{
+				DrawHelpers.DrawOutline(default, default, default, default, (offset) =>
+					Main.EntitySpriteDraw(white, bellPosition + offset, bellSource, Projectile.GetAlpha(Color.Goldenrod.Additive(100)) * Charge * 0.25f, bellRotation, bellOrigin, TotalScale, Effects, 0));
+			}
+
+			Main.EntitySpriteDraw(texture, bellPosition, bellSource, Projectile.GetAlpha(lightColor), bellRotation, bellOrigin, TotalScale, Effects, 0);
 			Main.EntitySpriteDraw(texture, drawPosition, poleSource, Projectile.GetAlpha(lightColor), Projectile.rotation, HoldPoint, TotalScale, Effects, 0);
 
 			//Flash when fully charged
 			if (CheckAIState(AIStates.CHARGING) && _flickerTime > 0)
 			{
-				Texture2D flash = TextureColorCache.ColorSolid(texture, Color.White);
+				
 				float alpha = EaseFunction.EaseQuadIn.Ease(EaseFunction.EaseSine.Ease(_flickerTime / (float)MAX_FLICKERTIME));
 				var color = Color.Lerp(Color.RosyBrown, Color.White, alpha * alpha).Additive();
 
-				Main.EntitySpriteDraw(flash, bellPosition, bellSource, color * alpha * 2, Projectile.rotation + bellRotation, bellOrigin, TotalScale, Effects, 0);
-				Main.EntitySpriteDraw(flash, drawPosition, poleSource, color * alpha * 2, Projectile.rotation, HoldPoint, TotalScale, Effects, 0);
+				Main.EntitySpriteDraw(white, bellPosition, bellSource, color * alpha * 2, bellRotation, bellOrigin, TotalScale, Effects, 0);
+				Main.EntitySpriteDraw(white, drawPosition, poleSource, color * alpha * 2, Projectile.rotation, HoldPoint, TotalScale, Effects, 0);
 			}
-
-			_bellVelocity += new Vector2(-Math.Abs(Owner.velocity.X), Owner.velocity.Y) * Owner.direction / 20f;
-			_bellVelocity *= 0.95f;
 
 			return true;
 		}
