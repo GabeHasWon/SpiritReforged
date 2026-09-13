4a9baaf48d7bc4602ff3055256f74009cf94599b
GabeHasWon <gabehw.contact@gmail.com>
2026-09-12 22:35:40 -0500

    minor cleanup

--- Common/ItemCommon/Backpacks/BackpackPlayer.cs
+++ Common/ItemCommon/Backpacks/BackpackPlayer.cs
@@ -3,7 +3,6 @@ using SpiritReforged.Common.Multiplayer;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
-using Terraria;
 using Terraria.GameContent.Achievements;
 using Terraria.ModLoader.IO;
 
