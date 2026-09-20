15e32fb9f56ee964e50811769b3cf6d1dce40330
GabeHasWon <gabehw.contact@gmail.com>
2026-09-20 11:32:50 -0500

    add gen index

--- Common/ModCompat/Spooky/Generation/SpookyForestGeneration.cs
+++ Common/ModCompat/Spooky/Generation/SpookyForestGeneration.cs
@@ -1,10 +1,5 @@
 ﻿using SpiritReforged.Common.WorldGeneration.Micropasses;
 using SpiritReforged.Content.Crossmod.Spooky.Tiles;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Terraria.IO;
 using Terraria.WorldBuilding;
 
@@ -14,13 +9,13 @@ internal class SpookyForestGeneration : Micropass
 {
 	public override string WorldGenName => "Spooky Uncommon Pots";
 
-	public override int GetWorldGenIndexInsert(List<GenPass> tasks, ref bool afterIndex) => throw new NotImplementedException();
+	public override int GetWorldGenIndexInsert(List<GenPass> tasks, ref bool afterIndex) => tasks.FindIndex(x => x.Name == "Spooky Forest") + 1;
 
 	private static int SpookyTile(string name) => CrossMod.Spooky.Instance.Find<ModTile>(name).Type;
 
 	public override void Run(GenerationProgress progress, GameConfiguration config)
 	{
-		if (CrossMod.Spooky.TryCall(out Vector2 BiomePosition, "BiomePositions", "SpookyForest"))
+		if (CrossMod.Spooky.TryCall(out Vector2 BiomePosition, "BiomePositions", "SpookyBiomeCenter"))
 			return;
 
 		Point pos = BiomePosition.ToPoint();
